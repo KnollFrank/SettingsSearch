9d171a7af79563863056ed725a9ae97545d67555
Frank Knoll <Knoll_Frank@web.de>
2024-05-16 10:29:33 +0200

    preparing unit tests

---
+++ app/src/androidTest/java/com/bytehamster/preferencesearch/SimpleExampleTest.java
@@ -0,0 +1,23 @@
+package com.bytehamster.preferencesearch;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class SimpleExampleTest {
+
+    @Test
+    public void onSearchResultClicked() {
+        // Given
+
+        // When
+
+        // Then
+        assertThat(4 + 1, is(5));
+    }
+}
---
+++ app/src/test/java/com/bytehamster/preferencesearch/SimpleExampleTest.java
@@ -0,0 +1,20 @@
+package com.bytehamster.preferencesearch;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+public class SimpleExampleTest {
+
+    @Test
+    public void onSearchResultClicked() {
+        // Given
+
+        // When
+
+        // Then
+        assertThat(4 + 1, is(5));
+    }
+}
