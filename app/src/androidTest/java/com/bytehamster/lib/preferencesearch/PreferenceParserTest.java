688180359b07b97494fc6c93dbdc8a85f165ba36
Frank Knoll <Knoll_Frank@web.de>
2024-05-16 12:10:29 +0200

    refactoring

--- app/src/androidTest/java/com/bytehamster/lib/preferencesearch/PreferenceParserTest.java
+++ app/src/androidTest/java/com/bytehamster/lib/preferencesearch/PreferenceParserTest.java
@@ -4,6 +4,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.core.StringContains.containsString;
 
+import androidx.annotation.XmlRes;
+
 import com.bytehamster.preferencesearch.test.R;
 
 import org.junit.Test;
@@ -16,15 +18,11 @@ public class PreferenceParserTest {
     @Test
     public void shouldSearch() {
         // Given
-        final PreferenceParser preferenceParser = new PreferenceParser(TestUtils.getContext());
-        final SearchConfiguration searchConfiguration = new SearchConfiguration();
-        searchConfiguration.index(R.xml.prefs);
-        for (final SearchConfiguration.SearchIndexItem file : searchConfiguration.getFiles()) {
-            preferenceParser.addResourceFile(file);
-        }
+        final PreferenceParser preferenceParser = getPreferenceParser(R.xml.prefs);
+        final String keyword = "Switch";
 
         // When
-        final List<PreferenceItem> preferenceItems = preferenceParser.searchFor("Switch", true);
+        final List<PreferenceItem> preferenceItems = preferenceParser.searchFor(keyword, true);
 
         // Then
         final List<String> titles =
@@ -32,6 +30,14 @@ public class PreferenceParserTest {
                         .stream()
                         .map(result -> result.title)
                         .collect(Collectors.toList());
-        assertThat(titles, hasItem(containsString("Switch")));
+        assertThat(titles, hasItem(containsString(keyword)));
+    }
+
+    private static PreferenceParser getPreferenceParser(@XmlRes final int preferenceScreen) {
+        final PreferenceParser preferenceParser = new PreferenceParser(TestUtils.getContext());
+        final SearchConfiguration searchConfiguration = new SearchConfiguration();
+        searchConfiguration.index(preferenceScreen);
+        searchConfiguration.getFiles().forEach(preferenceParser::addResourceFile);
+        return preferenceParser;
     }
 }
