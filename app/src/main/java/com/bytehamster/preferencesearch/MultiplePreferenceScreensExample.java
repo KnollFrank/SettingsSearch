8516c09e9eeab53651a341190a902c39156a108d
Frank Knoll <Knoll_Frank@web.de>
2024-05-18 14:03:59 +0200

    adding a second preference screen to MultiplePreferenceScreensExample

--- app/src/main/java/com/bytehamster/preferencesearch/MultiplePreferenceScreensExample.java
+++ app/src/main/java/com/bytehamster/preferencesearch/MultiplePreferenceScreensExample.java
@@ -13,14 +13,14 @@ import com.bytehamster.lib.preferencesearch.SearchPreferenceResult;
 import com.bytehamster.lib.preferencesearch.SearchPreferenceResultListener;
 
 public class MultiplePreferenceScreensExample extends AppCompatActivity implements SearchPreferenceResultListener {
+
     private PrefsFragment prefsFragment;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        prefsFragment = new PrefsFragment();
+        this.prefsFragment = new PrefsFragment();
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(android.R.id.content, prefsFragment)
@@ -29,30 +29,30 @@ public class MultiplePreferenceScreensExample extends AppCompatActivity implemen
 
     @Override
     public void onSearchResultClicked(@NonNull final SearchPreferenceResult result) {
-        prefsFragment = new PrefsFragment();
+        this.prefsFragment = new PrefsFragment();
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(android.R.id.content, prefsFragment)
                 .addToBackStack("PrefsFragment")
                 .commit(); // Allow to navigate back to search
-
         // Allow fragment to get created
         new Handler().post(() -> prefsFragment.onSearchResultClicked(result));
     }
 
     public static class PrefsFragment extends PreferenceFragmentCompat {
+
         SearchPreference searchPreference;
 
         @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            addPreferencesFromResource(R.xml.preferences);
+        public void onCreatePreferences(final Bundle savedInstanceState, final String rootKey) {
+            addPreferencesFromResource(R.xml.preferences_multiple_screens);
 
-            searchPreference = findPreference("searchPreference");
-            SearchConfiguration config = searchPreference.getSearchConfiguration();
+            this.searchPreference = findPreference("searchPreference");
+            final SearchConfiguration config = searchPreference.getSearchConfiguration();
             config.setActivity((AppCompatActivity) getActivity());
             config.setFragmentContainerViewId(android.R.id.content);
 
-            config.index(R.xml.preferences).addBreadcrumb("Main file");
+            config.index(R.xml.preferences_multiple_screens).addBreadcrumb("Main file");
             config.index(R.xml.preferences2).addBreadcrumb("Second file");
             config.setBreadcrumbsEnabled(true);
             config.setHistoryEnabled(true);
@@ -70,6 +70,14 @@ public class MultiplePreferenceScreensExample extends AppCompatActivity implemen
                 addPreferencesFromResource(R.xml.preferences2);
                 result.highlight(this);
             }
+        }
+    }
+
+    public static class PrefsFragmentSecond extends PreferenceFragmentCompat {
+
+        @Override
+        public void onCreatePreferences(final Bundle savedInstanceState, final String rootKey) {
+            addPreferencesFromResource(R.xml.preferences2);
         }
     }
 }
