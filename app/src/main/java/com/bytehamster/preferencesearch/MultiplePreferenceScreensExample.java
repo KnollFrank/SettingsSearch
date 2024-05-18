1c1dd6eb01a2f3e20453fe0c0db0f3138309522c
Frank Knoll <Knoll_Frank@web.de>
2024-05-18 13:33:47 +0200

    starting MultiplePreferenceScreensExample

---
+++ app/src/main/java/com/bytehamster/preferencesearch/MultiplePreferenceScreensExample.java
@@ -0,0 +1,75 @@
+package com.bytehamster.preferencesearch;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.bytehamster.lib.preferencesearch.SearchConfiguration;
+import com.bytehamster.lib.preferencesearch.SearchPreference;
+import com.bytehamster.lib.preferencesearch.SearchPreferenceResult;
+import com.bytehamster.lib.preferencesearch.SearchPreferenceResultListener;
+
+public class MultiplePreferenceScreensExample extends AppCompatActivity implements SearchPreferenceResultListener {
+    private PrefsFragment prefsFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        prefsFragment = new PrefsFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(android.R.id.content, prefsFragment)
+                .commit();
+    }
+
+    @Override
+    public void onSearchResultClicked(@NonNull final SearchPreferenceResult result) {
+        prefsFragment = new PrefsFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(android.R.id.content, prefsFragment)
+                .addToBackStack("PrefsFragment")
+                .commit(); // Allow to navigate back to search
+
+        // Allow fragment to get created
+        new Handler().post(() -> prefsFragment.onSearchResultClicked(result));
+    }
+
+    public static class PrefsFragment extends PreferenceFragmentCompat {
+        SearchPreference searchPreference;
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            addPreferencesFromResource(R.xml.preferences);
+
+            searchPreference = findPreference("searchPreference");
+            SearchConfiguration config = searchPreference.getSearchConfiguration();
+            config.setActivity((AppCompatActivity) getActivity());
+            config.setFragmentContainerViewId(android.R.id.content);
+
+            config.index(R.xml.preferences).addBreadcrumb("Main file");
+            config.index(R.xml.preferences2).addBreadcrumb("Second file");
+            config.setBreadcrumbsEnabled(true);
+            config.setHistoryEnabled(true);
+            config.setFuzzySearchEnabled(true);
+        }
+
+        private void onSearchResultClicked(final SearchPreferenceResult result) {
+            if (result.getResourceFile() == R.xml.preferences) {
+                searchPreference.setVisible(false); // Do not allow to click search multiple times
+                scrollToPreference(result.getKey());
+                findPreference(result.getKey()).setTitle("RESULT: " + findPreference(result.getKey()).getTitle());
+            } else {
+                // Result was found in the other file
+                getPreferenceScreen().removeAll();
+                addPreferencesFromResource(R.xml.preferences2);
+                result.highlight(this);
+            }
+        }
+    }
+}
