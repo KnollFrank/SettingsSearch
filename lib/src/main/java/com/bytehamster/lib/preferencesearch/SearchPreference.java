d9646503144f68f6a745784291cb90366805b411
Frank Knoll <Knoll_Frank@web.de>
2024-05-29 10:38:05 +0200

    adding a TODO

--- lib/src/main/java/com/bytehamster/lib/preferencesearch/SearchPreference.java
+++ lib/src/main/java/com/bytehamster/lib/preferencesearch/SearchPreference.java
@@ -10,6 +10,7 @@ import android.widget.EditText;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceViewHolder;
 
+// FK-TODO: Das Suchfeld für die Preferences soll selbst keine Preference sein, die weggescrollt werden kann, sondern soll immer am oberen Bildschirmrand sichtbar bleiben wie in den Systemeinstellungen.
 public class SearchPreference extends Preference implements View.OnClickListener {
     private final SearchConfiguration searchConfiguration = new SearchConfiguration();
     private String hint = null;
