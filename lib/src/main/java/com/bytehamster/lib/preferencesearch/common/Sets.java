7dec07c1faca92b630c14b261f218deac61aeff5
frankknoll <Knoll_Frank@web.de>
2024-05-29 16:13:26 +0200

    refactoring

--- lib/src/main/java/com/bytehamster/lib/preferencesearch/common/Sets.java
+++ lib/src/main/java/com/bytehamster/lib/preferencesearch/common/Sets.java
@@ -3,15 +3,13 @@ package com.bytehamster.lib.preferencesearch.common;
 import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Sets {
 
     public static <T> Set<T> union(final Collection<Set<T>> sets) {
-        return union(sets.stream());
-    }
-
-    private static <T> Set<T> union(final Stream<Set<T>> streamOfSets) {
-        return streamOfSets.flatMap(Set::stream).collect(Collectors.toSet());
+        return sets
+                .stream()
+                .flatMap(Set::stream)
+                .collect(Collectors.toSet());
     }
 }
