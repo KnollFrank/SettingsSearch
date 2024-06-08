3148b47a87a47df9414800059b28987fff0caca1
Frank Knoll <Knoll_Frank@web.de>
2024-06-08 17:49:27 +0200

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/preferencesearch/SearchResultsPreferenceFragment.java
+++ lib/src/main/java/de/KnollFrank/lib/preferencesearch/SearchResultsPreferenceFragment.java
@@ -20,47 +20,53 @@ public class SearchResultsPreferenceFragment extends BaseSearchPreferenceFragmen
     private List<Preference> preferences = Collections.emptyList();
 
     public void setPreferences(final List<Preference> preferences) {
-        removePreferencesFromTheirParents(preferences);
-        setClickListener(
+        PreferencesRemover.removePreferencesFromTheirParents(preferences);
+        ClickListenerSetter.setClickListener(
                 preference -> System.out.println("clicking on preference " + preference),
                 preferences);
         this
                 .getOptionalPreferenceScreen()
-                .ifPresent(preferenceScreen -> setPreferences(preferences, preferenceScreen));
+                .ifPresent(preferenceScreen -> PreferencesSetter.setPreferencesOnPreferenceScreen(preferences, preferenceScreen));
         this.preferences = preferences;
     }
 
     @Override
     public void onCreatePreferences(final Bundle savedInstanceState, final String rootKey) {
         final PreferenceScreen preferenceScreen = createPreferenceScreen();
-        addPreferences(preferences, preferenceScreen);
+        PreferencesSetter.addPreferences2PreferenceScreen(this.preferences, preferenceScreen);
         setPreferenceScreen(preferenceScreen);
     }
 
-    private static void removePreferencesFromTheirParents(final Collection<Preference> preferences) {
-        preferences.forEach(SearchResultsPreferenceFragment::removePreferenceFromItsParent);
-    }
+    private static class PreferencesRemover {
 
-    private static void removePreferenceFromItsParent(final Preference preference) {
-        final PreferenceGroup parent = preference.getParent();
-        if (parent != null) {
-            parent.removePreference(preference);
+        public static void removePreferencesFromTheirParents(final Collection<Preference> preferences) {
+            preferences.forEach(PreferencesRemover::removePreferenceFromItsParent);
+        }
+
+        private static void removePreferenceFromItsParent(final Preference preference) {
+            final PreferenceGroup parent = preference.getParent();
+            if (parent != null) {
+                parent.removePreference(preference);
+            }
         }
     }
 
-    private static void setClickListener(final Consumer<Preference> clickListener,
-                                         final List<Preference> preferences) {
-        for (final Preference preference : preferences) {
-            setClickListener(clickListener, preference);
-        }
-    }
+    private static class ClickListenerSetter {
 
-    private static void setClickListener(final Consumer<Preference> clickListener,
-                                         final Preference preference) {
-        preference.setEnabled(false);
-        preference.setShouldDisableView(false);
-        if (preference instanceof IClickablePreference) {
-            ((IClickablePreference) preference).setClickListener(clickListener);
+        public static void setClickListener(final Consumer<Preference> clickListener,
+                                            final List<Preference> preferences) {
+            for (final Preference preference : preferences) {
+                setClickListener(clickListener, preference);
+            }
+        }
+
+        private static void setClickListener(final Consumer<Preference> clickListener,
+                                             final Preference preference) {
+            preference.setEnabled(false);
+            preference.setShouldDisableView(false);
+            if (preference instanceof IClickablePreference) {
+                ((IClickablePreference) preference).setClickListener(clickListener);
+            }
         }
     }
 
@@ -74,14 +80,17 @@ public class SearchResultsPreferenceFragment extends BaseSearchPreferenceFragmen
                 .map(PreferenceManager::getPreferenceScreen);
     }
 
-    private static void setPreferences(final List<Preference> preferences,
-                                       final PreferenceScreen preferenceScreen) {
-        preferenceScreen.removeAll();
-        addPreferences(preferences, preferenceScreen);
-    }
+    private static class PreferencesSetter {
 
-    private static void addPreferences(final List<Preference> preferences,
-                                       final PreferenceScreen preferenceScreen) {
-        preferences.forEach(preferenceScreen::addPreference);
+        public static void setPreferencesOnPreferenceScreen(final List<Preference> preferences,
+                                                            final PreferenceScreen preferenceScreen) {
+            preferenceScreen.removeAll();
+            addPreferences2PreferenceScreen(preferences, preferenceScreen);
+        }
+
+        public static void addPreferences2PreferenceScreen(final List<Preference> preferences,
+                                                           final PreferenceScreen preferenceScreen) {
+            preferences.forEach(preferenceScreen::addPreference);
+        }
     }
 }
