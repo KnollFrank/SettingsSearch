dee4b2ae6e741173c1e53ed205a8e161a325d721
Frank Knoll <Knoll_Frank@web.de>
2024-06-11 13:11:03 +0200

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/preferencesearch/SearchPreferenceFragment.java
+++ lib/src/main/java/de/KnollFrank/lib/preferencesearch/SearchPreferenceFragment.java
@@ -15,102 +15,102 @@ import de.KnollFrank.lib.preferencesearch.common.Keyboard;
 
 public class SearchPreferenceFragment extends Fragment {
 
-    private SearchConfiguration searchConfiguration;
+	private SearchConfiguration searchConfiguration;
 
-    public static SearchPreferenceFragment newInstance(final SearchConfiguration searchConfiguration) {
-        final SearchPreferenceFragment searchPreferenceFragment = new SearchPreferenceFragment();
-        searchPreferenceFragment.setArguments(SearchConfigurations.toBundle(searchConfiguration));
-        return searchPreferenceFragment;
-    }
+	public static SearchPreferenceFragment newInstance(final SearchConfiguration searchConfiguration) {
+		final SearchPreferenceFragment searchPreferenceFragment = new SearchPreferenceFragment();
+		searchPreferenceFragment.setArguments(SearchConfigurations.toBundle(searchConfiguration));
+		return searchPreferenceFragment;
+	}
 
-    public SearchPreferenceFragment() {
-        super(R.layout.searchpreference_fragment);
-    }
+	public SearchPreferenceFragment() {
+		super(R.layout.searchpreference_fragment);
+	}
 
-    @Override
-    public void onCreate(@Nullable final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        searchConfiguration = SearchConfigurations.fromBundle(requireArguments());
-    }
+	@Override
+	public void onCreate(@Nullable final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		searchConfiguration = SearchConfigurations.fromBundle(requireArguments());
+	}
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        final View view = requireView();
-        final List<PreferenceWithHost> preferenceWithHostList =
-                this
-                        .getPreferencesProvider(R.id.dummyFragmentContainerView)
-                        .getPreferences();
-        final SearchResultsPreferenceFragment searchResultsPreferenceFragment =
-                SearchResultsPreferenceFragment.newInstance(searchConfiguration.fragmentContainerViewId);
-        Navigation.show(
-                searchResultsPreferenceFragment,
-                false,
-                getChildFragmentManager(),
-                R.id.searchResultsFragmentContainerView);
-        {
-            final SearchView searchView = view.findViewById(R.id.searchView);
-            SearchViewConfigurer.configureSearchView(
-                    searchView,
-                    searchResultsPreferenceFragment,
-                    new PreferenceSearcher<>(preferenceWithHostList),
-                    searchConfiguration);
-            selectSearchView(searchView);
-            searchView.setQuery(searchView.getQuery(), true);
-        }
-    }
+	@Override
+	public void onStart() {
+		super.onStart();
+		final View view = requireView();
+		final List<PreferenceWithHost> preferenceWithHostList =
+				this
+						.getPreferencesProvider(R.id.dummyFragmentContainerView)
+						.getPreferences();
+		final SearchResultsPreferenceFragment searchResultsPreferenceFragment =
+				SearchResultsPreferenceFragment.newInstance(searchConfiguration.fragmentContainerViewId);
+		Navigation.show(
+				searchResultsPreferenceFragment,
+				false,
+				getChildFragmentManager(),
+				R.id.searchResultsFragmentContainerView);
+		{
+			final SearchView searchView = view.findViewById(R.id.searchView);
+			SearchViewConfigurer.configureSearchView(
+					searchView,
+					searchResultsPreferenceFragment,
+					new PreferenceSearcher<>(preferenceWithHostList),
+					searchConfiguration);
+			selectSearchView(searchView);
+			searchView.setQuery(searchView.getQuery(), true);
+		}
+	}
 
-    private static class SearchViewConfigurer {
+	private static class SearchViewConfigurer {
 
-        private static void configureSearchView(final SearchView searchView,
-                                                final SearchResultsPreferenceFragment searchResultsPreferenceFragment,
-                                                final PreferenceSearcher<PreferenceWithHost> preferenceSearcher,
-                                                final SearchConfiguration searchConfiguration) {
-            searchConfiguration.textHint.ifPresent(searchView::setQueryHint);
-            searchView.setOnQueryTextListener(
-                    createOnQueryTextListener(
-                            searchResultsPreferenceFragment,
-                            preferenceSearcher));
-        }
+		public static void configureSearchView(final SearchView searchView,
+											   final SearchResultsPreferenceFragment searchResultsPreferenceFragment,
+											   final PreferenceSearcher<PreferenceWithHost> preferenceSearcher,
+											   final SearchConfiguration searchConfiguration) {
+			searchConfiguration.textHint.ifPresent(searchView::setQueryHint);
+			searchView.setOnQueryTextListener(
+					createOnQueryTextListener(
+							searchResultsPreferenceFragment,
+							preferenceSearcher));
+		}
 
-        private static OnQueryTextListener createOnQueryTextListener(
-                final SearchResultsPreferenceFragment searchResultsPreferenceFragment,
-                final PreferenceSearcher<PreferenceWithHost> preferenceSearcher) {
-            return new OnQueryTextListener() {
+		private static OnQueryTextListener createOnQueryTextListener(
+				final SearchResultsPreferenceFragment searchResultsPreferenceFragment,
+				final PreferenceSearcher<PreferenceWithHost> preferenceSearcher) {
+			return new OnQueryTextListener() {
 
-                @Override
-                public boolean onQueryTextSubmit(final String query) {
-                    onQueryTextChange(query);
-                    return false;
-                }
+				@Override
+				public boolean onQueryTextSubmit(final String query) {
+					onQueryTextChange(query);
+					return false;
+				}
 
-                @Override
-                public boolean onQueryTextChange(final String newText) {
-                    filterPreferenceItemsBy(newText);
-                    return true;
-                }
+				@Override
+				public boolean onQueryTextChange(final String newText) {
+					filterPreferenceItemsBy(newText);
+					return true;
+				}
 
-                private void filterPreferenceItemsBy(final String query) {
-                    searchResultsPreferenceFragment.setPreferenceWithHostList(
-                            preferenceSearcher.searchFor(query));
-                }
-            };
-        }
-    }
+				private void filterPreferenceItemsBy(final String query) {
+					searchResultsPreferenceFragment.setPreferenceWithHostList(
+							preferenceSearcher.searchFor(query));
+				}
+			};
+		}
+	}
 
-    private void selectSearchView(final SearchView searchView) {
-        searchView.requestFocus();
-        Keyboard.showKeyboard(getActivity(), searchView);
-    }
+	private void selectSearchView(final SearchView searchView) {
+		searchView.requestFocus();
+		Keyboard.showKeyboard(getActivity(), searchView);
+	}
 
-    private PreferencesProvider getPreferencesProvider(final @IdRes int fragmentContainerViewId) {
-        return new PreferencesProvider(
-                searchConfiguration.rootPreferenceFragment.getName(),
-                new PreferenceScreensProvider(
-                        new PreferenceFragments(
-                                requireActivity(),
-                                getChildFragmentManager(),
-                                fragmentContainerViewId)),
-                getContext());
-    }
+	private PreferencesProvider getPreferencesProvider(final @IdRes int fragmentContainerViewId) {
+		return new PreferencesProvider(
+				searchConfiguration.rootPreferenceFragment.getName(),
+				new PreferenceScreensProvider(
+						new PreferenceFragments(
+								requireActivity(),
+								getChildFragmentManager(),
+								fragmentContainerViewId)),
+				getContext());
+	}
 }
