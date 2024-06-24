7deebcf342970ee626dd5990fb060b4257e360e2
Frank Knoll <Knoll_Frank@web.de>
2024-06-24 21:41:13 +0200

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/preferencesearch/fragment/IFragmentFactory.java
+++ lib/src/main/java/de/KnollFrank/lib/preferencesearch/fragment/IFragmentFactory.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import androidx.fragment.app.Fragment;
 
+@FunctionalInterface
 public interface IFragmentFactory {
 
     Fragment instantiate(Context context, String fragmentClassName);
