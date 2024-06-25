bc578bb610398da4fc8dbb26f7e8dd4d325d059f
Frank Knoll <Knoll_Frank@web.de>
2024-06-25 11:45:47 +0200

    adding yet failing test SearchViewExampleTest.shouldSearchAndNotFindInvisiblePreference()

---
+++ app/src/androidTest/java/de/KnollFrank/preferencesearch/Matchers.java
@@ -0,0 +1,61 @@
+package de.KnollFrank.preferencesearch;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.Adapter;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+import androidx.test.espresso.matcher.BoundedMatcher;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+class Matchers {
+
+    // adapted from https://stackoverflow.com/a/53289078/12982352
+    public static Matcher<View> recyclerViewHasItem(final Matcher<View> matcher) {
+        return new BoundedMatcher<>(RecyclerView.class) {
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("has item: ");
+                matcher.describeTo(description);
+            }
+
+            @Override
+            protected boolean matchesSafely(final RecyclerView view) {
+                final Adapter adapter = view.getAdapter();
+                for (int position = 0; position < adapter.getItemCount(); position++) {
+                    final int type = adapter.getItemViewType(position);
+                    final ViewHolder holder = adapter.createViewHolder(view, type);
+                    adapter.onBindViewHolder(holder, position);
+                    if (matcher.matches(holder.itemView)) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        };
+    }
+
+    public static Matcher<View> childAtPosition(final Matcher<View> parentMatcher, final int position) {
+        return new TypeSafeMatcher<>() {
+
+            @Override
+            public void describeTo(final Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(final View view) {
+                final ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
--- app/src/androidTest/java/de/KnollFrank/preferencesearch/SearchViewExampleTest.java
+++ app/src/androidTest/java/de/KnollFrank/preferencesearch/SearchViewExampleTest.java
@@ -5,26 +5,26 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
+import static de.KnollFrank.preferencesearch.Matchers.childAtPosition;
+import static de.KnollFrank.preferencesearch.Matchers.recyclerViewHasItem;
 
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
-import org.hamcrest.Description;
 import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
@@ -39,10 +39,17 @@ public class SearchViewExampleTest {
             new ActivityScenarioRule<>(SearchViewExample.class);
 
     @Test
-    public void shouldSearchAndFindPreferences() {
+    public void shouldSearchAndFindPreference() {
         onView(searchButton()).perform(click());
         onView(searchView()).perform(replaceText("fourth"), closeSoftKeyboard());
-        onView(checkboxInSearchResults()).check(matches(withText("Checkbox fourth file")));
+        onView(recyclerView()).check(matches(recyclerViewHasItem(hasDescendant(withText("Checkbox fourth file")))));
+    }
+
+    @Test
+    public void shouldSearchAndNotFindInvisiblePreference() {
+        onView(searchButton()).perform(click());
+        onView(searchView()).perform(replaceText("invisible"), closeSoftKeyboard());
+        onView(recyclerView()).check(matches(not(recyclerViewHasItem(hasDescendant(withText("title of invisible preference"))))));
     }
 
     private static Matcher<View> searchButton() {
@@ -70,29 +77,13 @@ public class SearchViewExampleTest {
                 isDisplayed());
     }
 
-    private static Matcher<View> checkboxInSearchResults() {
+    private static Matcher<View> recyclerView() {
         return allOf(
-                withId(android.R.id.title),
-                withText("Checkbox fourth file"),
-                withParent(withParent(IsInstanceOf.instanceOf(android.widget.LinearLayout.class))),
+                withId(androidx.preference.R.id.recycler_view),
+                withParent(
+                        allOf(
+                                withId(android.R.id.list_container),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class)))),
                 isDisplayed());
-    }
-
-    private static Matcher<View> childAtPosition(final Matcher<View> parentMatcher, final int position) {
-        return new TypeSafeMatcher<>() {
-
-            @Override
-            public void describeTo(final Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(final View view) {
-                final ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
     }
 }
