75af3ff6900ea917646048a1bfadbd3437b19144
Frank Knoll <Knoll_Frank@web.de>
2024-06-27 01:40:18 +0200

    refactoring

---
+++ app/src/androidTest/java/de/KnollFrank/lib/preferencesearch/search/PreferenceFragment.java
@@ -0,0 +1,31 @@
+package de.KnollFrank.lib.preferencesearch.search;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
+
+import java.util.function.Function;
+
+public class PreferenceFragment extends PreferenceFragmentCompat {
+
+    private final Function<Context, Preference> preferenceFactory;
+
+    public PreferenceFragment(final Function<Context, Preference> preferenceFactory) {
+        this.preferenceFactory = preferenceFactory;
+    }
+
+    public static PreferenceFragment fromSinglePreference(final Function<Context, Preference> contextPreferenceFunction) {
+        return new PreferenceFragment(contextPreferenceFunction);
+    }
+
+    @Override
+    public void onCreatePreferences(final Bundle savedInstanceState, final String rootKey) {
+        final Context context = getPreferenceManager().getContext();
+        final PreferenceScreen screen = getPreferenceManager().createPreferenceScreen(context);
+        screen.addPreference(preferenceFactory.apply(context));
+        setPreferenceScreen(screen);
+    }
+}
--- app/src/androidTest/java/de/KnollFrank/lib/preferencesearch/search/PreferenceSearcherTest.java
+++ app/src/androidTest/java/de/KnollFrank/lib/preferencesearch/search/PreferenceSearcherTest.java
@@ -5,33 +5,33 @@ import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.core.StringContains.containsString;
 
-import android.content.Context;
-import android.os.Bundle;
 import android.os.Looper;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.preference.CheckBoxPreference;
 import androidx.preference.ListPreference;
 import androidx.preference.MultiSelectListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
-import androidx.preference.PreferenceScreen;
 import androidx.test.core.app.ActivityScenario;
 
 import org.hamcrest.Matcher;
-import org.junit.AfterClass;
+import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
-import de.KnollFrank.lib.preferencesearch.FragmentsFactory;
 import de.KnollFrank.lib.preferencesearch.MergedPreferenceScreen;
 import de.KnollFrank.lib.preferencesearch.PreferenceScreenWithHostProvider;
 import de.KnollFrank.lib.preferencesearch.PreferenceScreensProvider;
 import de.KnollFrank.lib.preferencesearch.common.Preferences;
 import de.KnollFrank.lib.preferencesearch.fragment.Fragments;
+import de.KnollFrank.lib.preferencesearch.fragment.FragmentsFactory;
 import de.KnollFrank.lib.preferencesearch.provider.MergedPreferenceScreenProvider;
 import de.KnollFrank.lib.preferencesearch.provider.PreferenceScreensMerger;
 import de.KnollFrank.preferencesearch.test.TestActivity;
@@ -43,129 +43,136 @@ public class PreferenceSearcherTest {
         Looper.prepare();
     }
 
-    @AfterClass
-    public static void afterClass() {
-        Looper.getMainLooper().quitSafely();
+    @After
+    public void afterClass() {
+        // Looper.getMainLooper().quitSafely();
     }
 
     @Test
     public void shouldSearchAndFind() {
-        final String keyword = PrefsFragment.SEARCH_QUERY_FOR_SOME_PREFERENCE;
-        testSearch(PrefsFragment.class, keyword, hasItem(containsString(keyword)));
+        final String keyword = "fourth";
+        final String title = String.format("Checkbox %s file", keyword);
+        testSearch(
+                PreferenceFragment.fromSinglePreference(
+                        context -> {
+                            final CheckBoxPreference checkBoxPreference = new CheckBoxPreference(context);
+                            checkBoxPreference.setKey("fourthfile");
+                            checkBoxPreference.setSummary(
+                                    String.format(
+                                            "This checkbox is a preference coming from a %s file",
+                                            keyword));
+                            checkBoxPreference.setTitle(title);
+                            return checkBoxPreference;
+                        }),
+                keyword,
+                hasItem(containsString(title)));
     }
 
     @Test
     public void shouldSearchAndFindListPreference() {
+        final String keyword = "entry of some ListPreference";
+        final String title = "Select list preference";
         testSearch(
-                PrefsFragment.class,
-                PrefsFragment.SEARCH_QUERY_FOR_SOME_ENTRY_OF_A_LIST_PREFERENCE,
-                hasItem(containsString("Select list preference")));
+                PreferenceFragment.fromSinglePreference(
+                        context -> {
+                            final ListPreference listPreference = new ListPreference(context);
+                            listPreference.setKey("keyOfSomeListPreference");
+                            listPreference.setSummary("This allows to select from a list");
+                            listPreference.setTitle(title);
+                            listPreference.setEntries(new String[]{keyword});
+                            return listPreference;
+                        }),
+                keyword,
+                hasItem(containsString(title)));
     }
 
     @Test
     public void shouldSearchAndFindMultiSelectListPreference() {
+        final String keyword = "entry of some MultiSelectListPreference";
+        final String title = "Multi select list preference";
         testSearch(
-                PrefsFragment.class,
-                PrefsFragment.SEARCH_QUERY_FOR_SOME_ENTRY_OF_A_MULTI_SELECT_LIST_PREFERENCE,
-                hasItem(containsString("Multi select list preference")));
+                PreferenceFragment.fromSinglePreference(
+                        context -> {
+                            final MultiSelectListPreference multiSelectListPreference = new MultiSelectListPreference(context);
+                            multiSelectListPreference.setKey("keyOfSomeMultiSelectListPreference");
+                            multiSelectListPreference.setSummary("This allows to select multiple entries from a list");
+                            multiSelectListPreference.setTitle(title);
+                            multiSelectListPreference.setEntries(new String[]{keyword});
+                            return multiSelectListPreference;
+                        }),
+                keyword,
+                hasItem(containsString(title)));
     }
 
     @Test
     public void shouldSearchAndNotFind() {
-        final String keyword = PrefsFragment.SEARCH_QUERY_FOR_SOME_NON_EXISTING_PREFERENCE;
-        testSearch(PrefsFragment.class, keyword, not(hasItem(containsString(keyword))));
+        final String keyword = "non_existing_keyword";
+        testSearch(
+                PreferenceFragment.fromSinglePreference(
+                        context -> {
+                            final CheckBoxPreference checkBoxPreference = new CheckBoxPreference(context);
+                            checkBoxPreference.setKey("fourthfile");
+                            checkBoxPreference.setSummary("This checkbox is a preference coming from a fourth file");
+                            checkBoxPreference.setTitle("Checkbox fourth file");
+                            return checkBoxPreference;
+                        }),
+                keyword,
+                not(hasItem(containsString(keyword))));
     }
 
-    public static class PrefsFragment extends PreferenceFragmentCompat {
-
-        public static final String SEARCH_QUERY_FOR_SOME_PREFERENCE = "fourth";
-        public static final String SEARCH_QUERY_FOR_SOME_NON_EXISTING_PREFERENCE = "non_existing_keyword";
-        public static final String SEARCH_QUERY_FOR_SOME_ENTRY_OF_A_LIST_PREFERENCE = "entry of some ListPreference";
-        public static final String SEARCH_QUERY_FOR_SOME_ENTRY_OF_A_MULTI_SELECT_LIST_PREFERENCE = "entry of some MultiSelectListPreference";
-
-        @Override
-        public void onCreatePreferences(final Bundle savedInstanceState, final String rootKey) {
-            final Context context = getPreferenceManager().getContext();
-            final PreferenceScreen screen = getPreferenceManager().createPreferenceScreen(context);
-            screen.addPreference(createPreference(context));
-            screen.addPreference(createListPreference(context));
-            screen.addPreference(createMultiSelectListPreference(context));
-            setPreferenceScreen(screen);
-        }
-
-        // FK-TODO: allow to create a Preference within test methods via a callback of type Context -> Preference
-        private static Preference createPreference(final Context context) {
-            final CheckBoxPreference checkBoxPreference = new CheckBoxPreference(context);
-            checkBoxPreference.setKey("fourthfile");
-            checkBoxPreference.setSummary(String.format("This checkbox is a preference coming from a %s file", SEARCH_QUERY_FOR_SOME_PREFERENCE));
-            checkBoxPreference.setTitle(String.format("Checkbox %s file", SEARCH_QUERY_FOR_SOME_PREFERENCE));
-            return checkBoxPreference;
-        }
-
-        private static Preference createListPreference(final Context context) {
-            final ListPreference listPreference = new ListPreference(context);
-            listPreference.setKey("keyOfSomeListPreference");
-            listPreference.setSummary("This allows to select from a list");
-            listPreference.setTitle("Select list preference");
-            listPreference.setEntries(new String[]{SEARCH_QUERY_FOR_SOME_ENTRY_OF_A_LIST_PREFERENCE});
-            return listPreference;
-        }
-
-        private static Preference createMultiSelectListPreference(final Context context) {
-            final MultiSelectListPreference multiSelectListPreference = new MultiSelectListPreference(context);
-            multiSelectListPreference.setKey("keyOfSomeMultiSelectListPreference");
-            multiSelectListPreference.setSummary("This allows to select multiple entries from a list");
-            multiSelectListPreference.setTitle("Multi select list preference");
-            multiSelectListPreference.setEntries(new String[]{SEARCH_QUERY_FOR_SOME_ENTRY_OF_A_MULTI_SELECT_LIST_PREFERENCE});
-            return multiSelectListPreference;
-        }
-    }
-
-    private static void testSearch(final Class<? extends PreferenceFragmentCompat> preferenceScreen,
+    private static void testSearch(final PreferenceFragmentCompat preferenceFragment,
                                    final String keyword,
-                                   final Matcher<Iterable<? super String>> titlesMatcher) {
+                                   final Matcher<Iterable<? super String>> titleMatcher) {
         try (final ActivityScenario<TestActivity> scenario = ActivityScenario.launch(TestActivity.class)) {
             scenario.onActivity(fragmentActivity -> {
                 // Given
                 final PreferenceSearcher preferenceSearcher =
                         new PreferenceSearcher(
-                                getPreferences(preferenceScreen, fragmentActivity));
+                                getPreferences(preferenceFragment, fragmentActivity));
 
                 // When
                 final List<PreferenceMatch> preferenceMatches = preferenceSearcher.searchFor(keyword);
 
                 // Then
-                // FK-TODO: replace titlesMatcher with a preferenceKeyMatcher?
-                assertThat(getTitles(preferenceMatches), titlesMatcher);
+                // FK-TODO: replace titleMatcher with a preferenceKeyMatcher?
+                assertThat(getTitles(preferenceMatches), titleMatcher);
             });
         }
     }
 
-    private static List<Preference> getPreferences(final Class<? extends PreferenceFragmentCompat> preferenceScreen,
-                                                   final TestActivity fragmentActivity) {
-        final MergedPreferenceScreen mergedPreferenceScreen = getMergedPreferenceScreen(preferenceScreen, fragmentActivity);
+    private static List<Preference> getPreferences(final PreferenceFragmentCompat preferenceFragment,
+                                                   final FragmentActivity fragmentActivity) {
+        final MergedPreferenceScreen mergedPreferenceScreen = getMergedPreferenceScreen(preferenceFragment, fragmentActivity);
         return Preferences.getAllPreferences(mergedPreferenceScreen.preferenceScreen);
     }
 
     private static MergedPreferenceScreen getMergedPreferenceScreen(
-            final Class<? extends PreferenceFragmentCompat> preferenceScreen,
-            final TestActivity fragmentActivity) {
-        final Fragments fragments = FragmentsFactory.createFragments(fragmentActivity);
+            final PreferenceFragmentCompat preferenceFragment,
+            final FragmentActivity fragmentActivity) {
+        final Fragments fragments =
+                FragmentsFactory.createFragments(
+                        (fragmentClassName, context) ->
+                                fragmentClassName.equals(preferenceFragment.getClass().getName()) ?
+                                        preferenceFragment :
+                                        Fragment.instantiate(context, fragmentClassName),
+                        fragmentActivity,
+                        fragmentActivity.getSupportFragmentManager(),
+                        TestActivity.FRAGMENT_CONTAINER_VIEW);
         final MergedPreferenceScreenProvider mergedPreferenceScreenProvider =
                 new MergedPreferenceScreenProvider(
                         fragments,
                         new PreferenceScreensProvider(new PreferenceScreenWithHostProvider(fragments)),
                         new PreferenceScreensMerger(fragmentActivity));
-        return mergedPreferenceScreenProvider.getMergedPreferenceScreen(preferenceScreen.getName());
+        return mergedPreferenceScreenProvider.getMergedPreferenceScreen(preferenceFragment.getClass().getName());
     }
 
-    private static List<String> getTitles(final List<PreferenceMatch> preferenceMatches) {
+    private static Set<String> getTitles(final List<PreferenceMatch> preferenceMatches) {
         return preferenceMatches
                 .stream()
                 .map(preferenceMatch -> preferenceMatch.preference)
                 .map(Preference::getTitle)
                 .filter(Objects::nonNull)
                 .map(CharSequence::toString)
-                .collect(Collectors.toList());
+                .collect(Collectors.toSet());
     }
 }
