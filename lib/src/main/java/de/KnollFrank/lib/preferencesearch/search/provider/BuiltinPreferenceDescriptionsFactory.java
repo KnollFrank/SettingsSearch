b0f23ba2b4eb52bd9335c4271d1c0be0c96232ab
Frank Knoll <Knoll_Frank@web.de>
2024-07-07 10:34:04 +0200

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/preferencesearch/search/provider/BuiltinPreferenceDescriptionsFactory.java
+++ lib/src/main/java/de/KnollFrank/lib/preferencesearch/search/provider/BuiltinPreferenceDescriptionsFactory.java
@@ -7,23 +7,56 @@ import androidx.preference.SwitchPreference;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
+import de.KnollFrank.lib.preferencesearch.common.Strings;
 
 public class BuiltinPreferenceDescriptionsFactory {
 
     // FK-TODO: hier noch andere Preferences behandeln: SwitchPreference, ...
     public static List<PreferenceDescription> createBuiltinPreferenceDescriptions() {
         return Arrays.asList(
-                new PreferenceDescription<>(
-                        ListPreference.class,
-                        new ListPreferenceSearchableInfoProvider()),
-                new PreferenceDescription<>(
-                        SwitchPreference.class,
-                        new SwitchPreferenceSearchableInfoProvider()),
-                new PreferenceDescription<>(
-                        DropDownPreference.class,
-                        new DropDownPreferenceSearchableInfoProvider()),
-                new PreferenceDescription<>(
-                        MultiSelectListPreference.class,
-                        new MultiSelectListPreferenceSearchableInfoProvider()));
+                getListPreferenceDescription(),
+                getSwitchPreferenceDescription(),
+                getDropDownPreferenceDescription(),
+                getMultiSelectListPreferenceDescription());
+    }
+
+    private static PreferenceDescription<ListPreference> getListPreferenceDescription() {
+        return new PreferenceDescription<>(
+                ListPreference.class,
+                preference ->
+                        Strings.join(
+                                ", ",
+                                Optional.ofNullable(preference.getEntries())));
+    }
+
+    private static PreferenceDescription<SwitchPreference> getSwitchPreferenceDescription() {
+        return new PreferenceDescription<>(
+                SwitchPreference.class,
+                preference ->
+                        Strings.joinNonNullElements(
+                                ", ",
+                                Arrays.asList(
+                                        preference.getSummaryOff(),
+                                        preference.getSummaryOn())));
+    }
+
+    private static PreferenceDescription<DropDownPreference> getDropDownPreferenceDescription() {
+        return new PreferenceDescription<>(
+                DropDownPreference.class,
+                preference ->
+                        Strings.join(
+                                ", ",
+                                Optional.ofNullable(preference.getEntries())));
+    }
+
+    private static PreferenceDescription<MultiSelectListPreference> getMultiSelectListPreferenceDescription() {
+        return new PreferenceDescription<>(
+                MultiSelectListPreference.class,
+                preference ->
+                        Strings.join(
+                                ", ",
+                                Optional.ofNullable(preference.getEntries())));
     }
 }
