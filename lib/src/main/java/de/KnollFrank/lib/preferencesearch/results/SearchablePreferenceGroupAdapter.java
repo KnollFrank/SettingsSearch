806eca91c14b5163b4ccb0a05850304ea88bf202
Frank Knoll <Knoll_Frank@web.de>
2024-07-08 10:07:34 +0200

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/preferencesearch/results/SearchablePreferenceGroupAdapter.java
+++ lib/src/main/java/de/KnollFrank/lib/preferencesearch/results/SearchablePreferenceGroupAdapter.java
@@ -1,11 +1,7 @@
 package de.KnollFrank.lib.preferencesearch.results;
 
 import android.content.Context;
-import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.preference.Preference;
@@ -13,14 +9,11 @@ import androidx.preference.PreferenceGroup;
 import androidx.preference.PreferenceGroupAdapter;
 import androidx.preference.PreferenceViewHolder;
 
-import java.util.Optional;
 import java.util.function.Consumer;
 
 import de.KnollFrank.lib.preferencesearch.search.provider.SearchableInfoGetter;
 
 class SearchablePreferenceGroupAdapter extends PreferenceGroupAdapter {
-
-    public static final int SEARCHABLE_INFO_VIEW_ID = View.generateViewId();
 
     private final SearchableInfoGetter searchableInfoGetter;
     private final Consumer<Preference> onPreferenceClickListener;
@@ -37,7 +30,7 @@ class SearchablePreferenceGroupAdapter extends PreferenceGroupAdapter {
     @Override
     public PreferenceViewHolder onCreateViewHolder(@NonNull final ViewGroup parent, final int viewType) {
         final PreferenceViewHolder preferenceViewHolder = super.onCreateViewHolder(parent, viewType);
-        addSearchableInfoView(parent, preferenceViewHolder);
+        createSearchableInfoViewBelowSummary(preferenceViewHolder, parent.getContext());
         return preferenceViewHolder;
     }
 
@@ -45,68 +38,16 @@ class SearchablePreferenceGroupAdapter extends PreferenceGroupAdapter {
     public void onBindViewHolder(@NonNull final PreferenceViewHolder holder, final int position) {
         super.onBindViewHolder(holder, position);
         final Preference preference = getItem(position);
-        displaySearchableInfo(holder, preference);
+        SearchableInfoView.displaySearchableInfo(holder, searchableInfoGetter.getSearchableInfo(preference));
         UIUtils.setOnClickListener(
                 holder.itemView,
                 v -> onPreferenceClickListener.accept(preference));
     }
 
-    private void addSearchableInfoView(final ViewGroup parent, final PreferenceViewHolder preferenceViewHolder) {
-        addSecondViewBelowFirstView(
+    private static void createSearchableInfoViewBelowSummary(final PreferenceViewHolder preferenceViewHolder, final Context context) {
+        UIUtils.addSecondViewBelowFirstView(
                 preferenceViewHolder.findViewById(android.R.id.summary),
-                createSearchableInfoView("", parent.getContext()), parent.getContext());
-    }
-
-    private void addSecondViewBelowFirstView(final View firstView, final View secondView, final Context context) {
-        final LinearLayout container = createContainer(context, firstView.getLayoutParams());
-        replaceView(firstView, container);
-        firstView.setLayoutParams(
-                new LinearLayout.LayoutParams(
-                        LayoutParams.WRAP_CONTENT,
-                        LayoutParams.WRAP_CONTENT));
-        container.addView(firstView);
-        container.addView(secondView);
-    }
-
-    private void replaceView(final View oldView, final View newView) {
-        final ViewGroup oldViewParent = (ViewGroup) oldView.getParent();
-        final int indexOfOldView = oldViewParent.indexOfChild(oldView);
-        oldViewParent.removeViewAt(indexOfOldView);
-        oldViewParent.addView(newView, indexOfOldView);
-    }
-
-    // FK-TODO: move UI code to new class
-    private static LinearLayout createContainer(final Context context, final LayoutParams layoutParams) {
-        final LinearLayout container = new LinearLayout(context);
-        container.setLayoutParams(layoutParams);
-        container.setOrientation(LinearLayout.VERTICAL);
-        return container;
-    }
-
-    private static TextView createSearchableInfoView(final String text, final Context context) {
-        final TextView searchableInfoView = new TextView(context);
-        searchableInfoView.setLayoutParams(
-                new LinearLayout.LayoutParams(
-                        LayoutParams.WRAP_CONTENT,
-                        LayoutParams.WRAP_CONTENT));
-        searchableInfoView.setText(text);
-        searchableInfoView.setId(SEARCHABLE_INFO_VIEW_ID);
-        searchableInfoView.setVisibility(View.GONE);
-        return searchableInfoView;
-    }
-
-    private static TextView getSearchableInfoView(final PreferenceViewHolder holder) {
-        return (TextView) holder.findViewById(SEARCHABLE_INFO_VIEW_ID);
-    }
-
-    private void displaySearchableInfo(final PreferenceViewHolder holder, final Preference preference) {
-        final TextView searchableInfoView = getSearchableInfoView(holder);
-        final Optional<CharSequence> searchableInfo = searchableInfoGetter.getSearchableInfo(preference);
-        if (searchableInfo.isPresent()) {
-            searchableInfoView.setText(searchableInfo.get());
-            searchableInfoView.setVisibility(View.VISIBLE);
-        } else {
-            searchableInfoView.setVisibility(View.GONE);
-        }
+                SearchableInfoView.createSearchableInfoView("", context),
+                context);
     }
 }
--- lib/src/main/java/de/KnollFrank/lib/preferencesearch/results/UIUtils.java
+++ lib/src/main/java/de/KnollFrank/lib/preferencesearch/results/UIUtils.java
@@ -1,13 +1,25 @@
 package de.KnollFrank.lib.preferencesearch.results;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
 
 class UIUtils {
 
     public static void setOnClickListener(final View view, final View.OnClickListener onClickListener) {
         makeChildViewsNonClickable(view);
         view.setOnClickListener(onClickListener);
+    }
+
+    public static void addSecondViewBelowFirstView(final View firstView,
+                                                   final View secondView,
+                                                   final Context context) {
+        final LinearLayout container = createLinearLayout(context, firstView.getLayoutParams());
+        replaceView(firstView, container);
+        addView2LinearLayout(firstView, container);
+        addView2LinearLayout(secondView, container);
     }
 
     private static void makeChildViewsNonClickable(final View view) {
@@ -22,5 +34,28 @@ class UIUtils {
                 setClickableRecursive(viewGroup.getChildAt(i), clickable);
             }
         }
+    }
+
+    private static LinearLayout createLinearLayout(final Context context, final LayoutParams layoutParams) {
+        final LinearLayout container = new LinearLayout(context);
+        container.setLayoutParams(layoutParams);
+        container.setOrientation(LinearLayout.VERTICAL);
+        return container;
+    }
+
+    private static void replaceView(final View oldView, final View newView) {
+        final ViewGroup oldViewParent = (ViewGroup) oldView.getParent();
+        final int indexOfOldView = oldViewParent.indexOfChild(oldView);
+        oldViewParent.removeViewAt(indexOfOldView);
+        oldViewParent.addView(newView, indexOfOldView);
+    }
+
+    private static void addView2LinearLayout(final View view, final LinearLayout linearLayout) {
+        view.setLayoutParams(createLinearLayoutParams());
+        linearLayout.addView(view);
+    }
+
+    private static LinearLayout.LayoutParams createLinearLayoutParams() {
+        return new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
     }
 }
