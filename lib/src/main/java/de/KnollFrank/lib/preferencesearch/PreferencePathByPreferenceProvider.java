89c24e62227291d3d7739f9960dd31ef67b3ec97
Frank Knoll <Knoll_Frank@web.de>
2024-07-30 22:59:09 +0200

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/preferencesearch/PreferencePathByPreferenceProvider.java
+++ lib/src/main/java/de/KnollFrank/lib/preferencesearch/PreferencePathByPreferenceProvider.java
@@ -6,7 +6,6 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMap.Builder;
 
 import org.jgrapht.Graph;
-import org.jgrapht.traverse.BreadthFirstIterator;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -24,31 +23,35 @@ class PreferencePathByPreferenceProvider {
     }
 
     private static Map<PreferenceScreenWithHost, PreferencePath> getPreferencePathByPreferenceScreen(final Graph<PreferenceScreenWithHost, PreferenceEdge> preferenceScreenGraph) {
-        final BreadthFirstIterator<PreferenceScreenWithHost, PreferenceEdge> iterator = new BreadthFirstIterator<>(preferenceScreenGraph);
         final Map<PreferenceScreenWithHost, PreferencePath> preferencePathByPreferenceScreen = new HashMap<>();
-        // FK-TODO: introduce new iterator for graphs
-        while (iterator.hasNext()) {
-            final PreferenceScreenWithHost preferenceScreen = iterator.next();
-            final PreferenceScreenWithHost parentPreferenceScreen = iterator.getParent(preferenceScreen);
-            preferencePathByPreferenceScreen.put(
-                    preferenceScreen,
-                    parentPreferenceScreen != null ?
-                            getPreferencePath(preferenceScreenGraph, preferencePathByPreferenceScreen, preferenceScreen, parentPreferenceScreen) :
-                            new PreferencePath(Collections.emptyList()));
-        }
-        return preferencePathByPreferenceScreen;
-    }
+        new BreadthFirstVisitor() {
 
-    private static PreferencePath getPreferencePath(final Graph<PreferenceScreenWithHost, PreferenceEdge> preferenceScreenGraph,
-                                                    final Map<PreferenceScreenWithHost, PreferencePath> preferencePathByPreferenceScreen,
-                                                    final PreferenceScreenWithHost preferenceScreen,
-                                                    final PreferenceScreenWithHost parentPreferenceScreen) {
-        final PreferencePath parentPreferencePath = preferencePathByPreferenceScreen.get(parentPreferenceScreen);
-        final Preference preference =
-                preferenceScreenGraph
-                        .getEdge(parentPreferenceScreen, preferenceScreen)
-                        .preference;
-        return parentPreferencePath.add(preference);
+            @Override
+            protected void visitRootNode(final PreferenceScreenWithHost preferenceScreen) {
+                preferencePathByPreferenceScreen.put(
+                        preferenceScreen,
+                        new PreferencePath(Collections.emptyList()));
+            }
+
+            @Override
+            protected void visitInnerNode(final PreferenceScreenWithHost preferenceScreen,
+                                          final PreferenceScreenWithHost parentPreferenceScreen) {
+                preferencePathByPreferenceScreen.put(
+                        preferenceScreen,
+                        getPreferencePath(preferenceScreen, parentPreferenceScreen));
+            }
+
+            private PreferencePath getPreferencePath(final PreferenceScreenWithHost preferenceScreen,
+                                                     final PreferenceScreenWithHost parentPreferenceScreen) {
+                final PreferencePath parentPreferencePath = preferencePathByPreferenceScreen.get(parentPreferenceScreen);
+                final Preference preference =
+                        preferenceScreenGraph
+                                .getEdge(parentPreferenceScreen, preferenceScreen)
+                                .preference;
+                return parentPreferencePath.add(preference);
+            }
+        }.visit(preferenceScreenGraph);
+        return preferencePathByPreferenceScreen;
     }
 
     private static Map<Preference, PreferencePath> getPreferencePathByPreference(
