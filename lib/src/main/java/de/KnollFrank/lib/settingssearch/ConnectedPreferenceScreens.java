6ebc7f016af40dfff7c1063a05f893a24c914939
frankknoll <Knoll_Frank@web.de>
2024-10-03 00:40:59 +0200

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/ConnectedPreferenceScreens.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/ConnectedPreferenceScreens.java
@@ -9,15 +9,15 @@ import java.util.Set;
 
 public class ConnectedPreferenceScreens {
 
-    public final Graph<PreferenceScreenWithHost, PreferenceEdge> preferenceScreenGraph;
+    public final Graph<PreferenceScreenWithHost, PreferenceEdge> searchablePreferenceScreenGraph;
     public final Map<Preference, PreferencePath> preferencePathByPreference;
 
-    public ConnectedPreferenceScreens(final Graph<PreferenceScreenWithHost, PreferenceEdge> preferenceScreenGraph) {
-        this.preferenceScreenGraph = preferenceScreenGraph;
-        this.preferencePathByPreference = PreferencePathByPreferenceProvider.getPreferencePathByPreference(preferenceScreenGraph);
+    public ConnectedPreferenceScreens(final Graph<PreferenceScreenWithHost, PreferenceEdge> searchablePreferenceScreenGraph) {
+        this.searchablePreferenceScreenGraph = searchablePreferenceScreenGraph;
+        this.preferencePathByPreference = PreferencePathByPreferenceProvider.getPreferencePathByPreference(searchablePreferenceScreenGraph);
     }
 
     public Set<PreferenceScreenWithHost> getConnectedPreferenceScreens() {
-        return preferenceScreenGraph.vertexSet();
+        return searchablePreferenceScreenGraph.vertexSet();
     }
 }
