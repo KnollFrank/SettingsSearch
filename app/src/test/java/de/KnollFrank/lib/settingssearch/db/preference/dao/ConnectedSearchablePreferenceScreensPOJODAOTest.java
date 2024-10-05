2b6ca998bdaae02c07b25b9ce38b1932ad8edb5b
Frank Knoll <Knoll_Frank@web.de>
2024-10-05 18:40:48 +0200

    refactoring

--- app/src/test/java/de/KnollFrank/lib/settingssearch/db/preference/dao/ConnectedSearchablePreferenceScreensPOJODAOTest.java
+++ app/src/test/java/de/KnollFrank/lib/settingssearch/db/preference/dao/ConnectedSearchablePreferenceScreensPOJODAOTest.java
@@ -3,8 +3,6 @@ package de.KnollFrank.lib.settingssearch.db.preference.dao;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import androidx.preference.PreferenceFragmentCompat;
-
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
@@ -12,22 +10,15 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import de.KnollFrank.lib.settingssearch.db.preference.pojo.ConnectedSearchablePreferenceScreensPOJO;
-import de.KnollFrank.lib.settingssearch.db.preference.pojo.PreferencePathPOJO;
-import de.KnollFrank.lib.settingssearch.db.preference.pojo.PreferenceScreenWithHostClassPOJO;
-import de.KnollFrank.lib.settingssearch.db.preference.pojo.SearchablePreferencePOJO;
-import de.KnollFrank.lib.settingssearch.db.preference.pojo.SearchablePreferenceScreenPOJO;
 
 public class ConnectedSearchablePreferenceScreensPOJODAOTest {
 
     @Test
     public void shouldPersistAndLoadConnectedSearchablePreferenceScreensPOJO() {
         // Given
-        final ConnectedSearchablePreferenceScreensPOJO pojo = createSomeConnectedSearchablePreferenceScreensPOJO();
+        final ConnectedSearchablePreferenceScreensPOJO pojo = POJOTestFactory.createSomeConnectedSearchablePreferenceScreensPOJO();
         final OutputStream outputStream = new ByteArrayOutputStream();
 
         // When
@@ -36,51 +27,6 @@ public class ConnectedSearchablePreferenceScreensPOJODAOTest {
 
         // Then
         assertThat(pojoActual, is(pojo));
-    }
-
-    private static ConnectedSearchablePreferenceScreensPOJO createSomeConnectedSearchablePreferenceScreensPOJO() {
-        return new ConnectedSearchablePreferenceScreensPOJO(
-                Set.of(createSomePreferenceScreenWithHostClassPOJO()),
-                Map.of(createSomeSearchablePreferencePOJO(), createSomePreferencePathPOJO()));
-    }
-
-    private static PreferenceScreenWithHostClassPOJO createSomePreferenceScreenWithHostClassPOJO() {
-        return new PreferenceScreenWithHostClassPOJO(
-                createSomeSearchablePreferenceScreenPOJO(),
-                PreferenceFragmentCompat.class);
-    }
-
-    private static PreferencePathPOJO createSomePreferencePathPOJO() {
-        return new PreferencePathPOJO(List.of(createSomeSearchablePreferencePOJO()));
-    }
-
-    private static SearchablePreferencePOJO createSomeSearchablePreferencePOJO() {
-        return new SearchablePreferencePOJO(
-                "some key",
-                4711,
-                4712,
-                "some summary",
-                "some title",
-                4713,
-                "some fragment",
-                true,
-                "some searchableInfo",
-                List.of(
-                        new SearchablePreferencePOJO(
-                                "some key 2",
-                                4714,
-                                4715,
-                                "some summary 2",
-                                "some title 2",
-                                4716,
-                                "some fragment 2",
-                                true,
-                                "some searchableInfo 2",
-                                List.of())));
-    }
-
-    private static SearchablePreferenceScreenPOJO createSomeSearchablePreferenceScreenPOJO() {
-        return new SearchablePreferenceScreenPOJO(List.of(createSomeSearchablePreferencePOJO()));
     }
 
     private static InputStream convert(final OutputStream outputStream) {
---
+++ app/src/test/java/de/KnollFrank/lib/settingssearch/db/preference/dao/POJOTestFactory.java
@@ -0,0 +1,61 @@
+package de.KnollFrank.lib.settingssearch.db.preference.dao;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import de.KnollFrank.lib.settingssearch.db.preference.pojo.ConnectedSearchablePreferenceScreensPOJO;
+import de.KnollFrank.lib.settingssearch.db.preference.pojo.PreferencePathPOJO;
+import de.KnollFrank.lib.settingssearch.db.preference.pojo.PreferenceScreenWithHostClassPOJO;
+import de.KnollFrank.lib.settingssearch.db.preference.pojo.SearchablePreferencePOJO;
+import de.KnollFrank.lib.settingssearch.db.preference.pojo.SearchablePreferenceScreenPOJO;
+
+class POJOTestFactory {
+
+    public static ConnectedSearchablePreferenceScreensPOJO createSomeConnectedSearchablePreferenceScreensPOJO() {
+        return new ConnectedSearchablePreferenceScreensPOJO(
+                Set.of(createSomePreferenceScreenWithHostClassPOJO()),
+                Map.of(createSomeSearchablePreferencePOJO(), createSomePreferencePathPOJO()));
+    }
+
+    private static PreferenceScreenWithHostClassPOJO createSomePreferenceScreenWithHostClassPOJO() {
+        return new PreferenceScreenWithHostClassPOJO(
+                createSomeSearchablePreferenceScreenPOJO(),
+                PreferenceFragmentCompat.class);
+    }
+
+    private static PreferencePathPOJO createSomePreferencePathPOJO() {
+        return new PreferencePathPOJO(List.of(createSomeSearchablePreferencePOJO()));
+    }
+
+    private static SearchablePreferencePOJO createSomeSearchablePreferencePOJO() {
+        return new SearchablePreferencePOJO(
+                "some key",
+                4711,
+                4712,
+                "some summary",
+                "some title",
+                4713,
+                "some fragment",
+                true,
+                "some searchableInfo",
+                List.of(
+                        new SearchablePreferencePOJO(
+                                "some key 2",
+                                4714,
+                                4715,
+                                "some summary 2",
+                                "some title 2",
+                                4716,
+                                "some fragment 2",
+                                true,
+                                "some searchableInfo 2",
+                                List.of())));
+    }
+
+    private static SearchablePreferenceScreenPOJO createSomeSearchablePreferenceScreenPOJO() {
+        return new SearchablePreferenceScreenPOJO(List.of(createSomeSearchablePreferencePOJO()));
+    }
+}
