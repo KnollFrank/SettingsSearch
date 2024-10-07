41826ce2d0013f8988621e5d85e8ca5f609cd8ab
frankknoll <Knoll_Frank@web.de>
2024-10-07 20:14:11 +0200

    adding a TODO

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/ConnectedSearchablePreferenceScreens.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/ConnectedSearchablePreferenceScreens.java
@@ -14,6 +14,7 @@ import java.util.Set;
 
 public record ConnectedSearchablePreferenceScreens(
         Set<PreferenceScreenWithHostClass> connectedSearchablePreferenceScreens,
+        // FK-TODO: SearchablePreference statt Preference als Key der folgenden Map?
         Map<Preference, PreferencePath> preferencePathByPreference) {
 
     public static ConnectedSearchablePreferenceScreens fromSearchablePreferenceScreenGraph(final Graph<PreferenceScreenWithHostClass, PreferenceEdge> searchablePreferenceScreenGraph,
