a261c6535c70915262266d700680d207834200d1
frankknoll <Knoll_Frank@web.de>
2024-10-11 22:53:18 +0200

    refining SearchablePreferenceScreenProviderTest

--- app/src/test/java/de/KnollFrank/lib/settingssearch/SearchablePreferenceScreenProviderTest.java
+++ app/src/test/java/de/KnollFrank/lib/settingssearch/SearchablePreferenceScreenProviderTest.java
@@ -2,8 +2,8 @@ package de.KnollFrank.lib.settingssearch;
 
 import static org.hamcrest.Matchers.is;
 
-import androidx.fragment.app.FragmentActivity;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceScreen;
 import androidx.test.core.app.ActivityScenario;
@@ -18,8 +18,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
-import java.util.function.Function;
-
 import de.KnollFrank.lib.settingssearch.db.preference.converter.PreferenceFragmentTemplate;
 import de.KnollFrank.lib.settingssearch.provider.IsPreferenceSearchable;
 import de.KnollFrank.settingssearch.test.TestActivity;
@@ -29,46 +27,87 @@ public class SearchablePreferenceScreenProviderTest {
 
     @Test
     public void test_getPreferenceScreen_searchablePreference() {
-        test_getPreferenceScreen(true);
+        test_getPreferenceScreen(
+                SearchablePreferenceScreenProviderTest::addSinglePreferencesScreen,
+                true);
     }
 
     @Test
     public void test_getPreferenceScreen_nonSearchablePreference() {
-        test_getPreferenceScreen(false);
+        test_getPreferenceScreen(
+                SearchablePreferenceScreenProviderTest::addSinglePreferencesScreen,
+                false);
     }
 
-    private static void test_getPreferenceScreen(final boolean preferenceSearchable) {
+    @Test
+    public void test_getPreferenceScreen_nestedSearchablePreference() {
+        test_getPreferenceScreen(
+                SearchablePreferenceScreenProviderTest::addNestedPreference2Screen,
+                true);
+    }
+
+    @Test
+    public void test_getPreferenceScreen_nestedNonSearchablePreference() {
+        test_getPreferenceScreen(
+                SearchablePreferenceScreenProviderTest::addNestedPreference2Screen,
+                false);
+    }
+
+    private interface AddPreferences2Screen {
+
+        void addPreferences2Screen(final String keyOfSearchableOrNonSearchablePreference,
+                                   final PreferenceScreen screen,
+                                   final Context context);
+    }
+
+    private void test_getPreferenceScreen(final AddPreferences2Screen addPreferences2Screen,
+                                          final boolean preferenceSearchable) {
         try (final ActivityScenario<TestActivity> scenario = ActivityScenario.launch(TestActivity.class)) {
             scenario.onActivity(activity -> {
                 // Given
-                final String keyOfPreference = "somePreference";
-                final Function<Context, Preference> preferenceFactory =
-                        context -> {
-                            final Preference preference = new Preference(context);
-                            preference.setKey(keyOfPreference);
-                            return preference;
-                        };
-                final IsPreferenceSearchable isPreferenceSearchable = (preference, host) -> keyOfPreference.equals(preference.getKey()) && preferenceSearchable;
+                final String keyOfSearchableOrNonSearchablePreference = "somePreference";
                 final PreferenceFragmentCompat preferenceFragment =
-                        getPreferenceFragmentCompat(preferenceFactory, activity);
+                        initializeFragment(
+                                new PreferenceFragmentTemplate(
+                                        (screen, context) ->
+                                                addPreferences2Screen.addPreferences2Screen(
+                                                        keyOfSearchableOrNonSearchablePreference,
+                                                        screen,
+                                                        context)),
+                                activity);
                 final SearchablePreferenceScreenProvider searchablePreferenceScreenProvider =
-                        new SearchablePreferenceScreenProvider(isPreferenceSearchable);
+                        new SearchablePreferenceScreenProvider(
+                                new IsPreferenceSearchable() {
+
+                                    @Override
+                                    public boolean isPreferenceOfHostSearchable(final Preference preference, final PreferenceFragmentCompat host) {
+                                        return keyOfSearchableOrNonSearchablePreference.equals(preference.getKey()) ?
+                                                preferenceSearchable :
+                                                true;
+                                    }
+                                });
 
                 // When
                 final PreferenceScreen preferenceScreen = searchablePreferenceScreenProvider.getPreferenceScreen(preferenceFragment);
 
                 // Then
-                assertThat(preferenceScreen.findPreference(keyOfPreference) != null, is(preferenceSearchable));
+                assertThat(preferenceScreen.findPreference(keyOfSearchableOrNonSearchablePreference) != null, is(preferenceSearchable));
             });
         }
     }
 
-    private static PreferenceFragmentCompat getPreferenceFragmentCompat(final Function<Context, Preference> preferenceFactory,
-                                                                        final FragmentActivity activity) {
-        return initializeFragment(
-                new PreferenceFragmentTemplate(
-                        (screen, context) ->
-                                screen.addPreference(preferenceFactory.apply(context))),
-                activity);
+    private static void addSinglePreferencesScreen(final String keyOfSearchableOrNonSearchablePreference, final PreferenceScreen screen, final Context context) {
+        final Preference preference = new Preference(context);
+        preference.setKey(keyOfSearchableOrNonSearchablePreference);
+        screen.addPreference(preference);
+    }
+
+    private static void addNestedPreference2Screen(final String keyOfSearchableOrNonSearchablePreference, final PreferenceScreen screen, final Context context) {
+        final PreferenceCategory parent = new PreferenceCategory(context);
+        screen.addPreference(parent);
+
+        final Preference nestedSearchablePreference = new Preference(context);
+        nestedSearchablePreference.setKey(keyOfSearchableOrNonSearchablePreference);
+        parent.addPreference(nestedSearchablePreference);
     }
 }
