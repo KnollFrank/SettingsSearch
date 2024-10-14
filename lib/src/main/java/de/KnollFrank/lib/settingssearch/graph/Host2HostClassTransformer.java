bcd1d8fc12d69371f1eb9738e0c5781137096e83
frankknoll <Knoll_Frank@web.de>
2024-10-14 21:36:06 +0200

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/graph/Host2HostClassTransformer.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/graph/Host2HostClassTransformer.java
@@ -5,33 +5,16 @@ import org.jgrapht.Graph;
 import de.KnollFrank.lib.settingssearch.PreferenceEdge;
 import de.KnollFrank.lib.settingssearch.PreferenceScreenWithHost;
 import de.KnollFrank.lib.settingssearch.PreferenceScreenWithHostClass;
-import de.KnollFrank.lib.settingssearch.common.graph.GraphTransformer;
-import de.KnollFrank.lib.settingssearch.common.graph.GraphTransformerAlgorithm;
+import de.KnollFrank.lib.settingssearch.common.graph.NodesTransformer;
 
 class Host2HostClassTransformer {
 
     public static Graph<PreferenceScreenWithHostClass, PreferenceEdge> transformHost2HostClass(
             final Graph<PreferenceScreenWithHost, PreferenceEdge> preferenceScreenGraph) {
-        return GraphTransformerAlgorithm.transform(
+        return NodesTransformer.transformNodes(
                 preferenceScreenGraph,
-                PreferenceEdge.class,
-                createGraphTransformer());
-    }
-
-    private static GraphTransformer<PreferenceScreenWithHost, PreferenceEdge, PreferenceScreenWithHostClass, PreferenceEdge> createGraphTransformer() {
-        return new GraphTransformer<>() {
-
-            @Override
-            public PreferenceScreenWithHostClass transformNode(final PreferenceScreenWithHost node) {
-                return new PreferenceScreenWithHostClass(
-                        node.preferenceScreen(),
-                        node.host().getClass());
-            }
-
-            @Override
-            public PreferenceEdge transformEdge(final PreferenceEdge edge, final PreferenceScreenWithHostClass transformedParentNode) {
-                return new PreferenceEdge(edge.preference);
-            }
-        };
+                preferenceScreenWithHost -> new PreferenceScreenWithHostClass(
+                        preferenceScreenWithHost.preferenceScreen(),
+                        preferenceScreenWithHost.host().getClass()));
     }
 }
