c918b7f07a447169b48b81cfb305b26c2d1c9df2
frankknoll <Knoll_Frank@web.de>
2024-10-19 23:26:48 +0200

    removing @Exclude annotation

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/dao/JsonDAO.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/dao/JsonDAO.java
@@ -33,7 +33,6 @@ public class JsonDAO {
         return new GsonBuilder()
                 .registerTypeAdapter(Class.class, new ClassTypeAdapter())
                 .registerTypeAdapterFactory(new BundleTypeAdapterFactory())
-                .setExclusionStrategies(new AnnotationExclusionStrategy())
                 .enableComplexMapKeySerialization()
                 .create();
     }
