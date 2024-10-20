0acca64c5c9a528a1fe793396f7671b55d9ff701
frankknoll <Knoll_Frank@web.de>
2024-10-20 22:00:31 +0200

    adding a TODO

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/SearchablePreferencePOJO.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/SearchablePreferencePOJO.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+// FK-TODO: add parent SearchablePreferencePOJO
 public record SearchablePreferencePOJO(
         int id,
         String key,
