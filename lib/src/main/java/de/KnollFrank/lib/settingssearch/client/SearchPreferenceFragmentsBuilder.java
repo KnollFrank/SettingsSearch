ba22b35966943fcb7d582e25ad8632ac7f768879
frankknoll <Knoll_Frank@web.de>
2024-11-04 23:47:18 +0100

    correcting a typo

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/client/SearchPreferenceFragmentsBuilder.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/client/SearchPreferenceFragmentsBuilder.java
@@ -18,7 +18,7 @@ import de.KnollFrank.lib.settingssearch.search.MergedPreferenceScreenDataInput;
 import de.KnollFrank.lib.settingssearch.search.MergedPreferenceScreenDataMode;
 import de.KnollFrank.lib.settingssearch.search.provider.SearchableInfoProvider;
 
-// FK-TODO aus OsmAnd: schalte über eine Benutzerdefinierte Bedingung ganze Gruppen von Preferences für die Suche an oder aus, z.B. Development aus OsmAnd.
+// FK-TODO aus OsmAnd: schalte über eine benutzerdefinierte Bedingung ganze Gruppen von Preferences für die Suche an oder aus, z.B. Development aus OsmAnd.
 public class SearchPreferenceFragmentsBuilder {
 
     private final SearchConfiguration searchConfiguration;
