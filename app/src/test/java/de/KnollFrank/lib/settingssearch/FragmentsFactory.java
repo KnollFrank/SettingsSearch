a9691624e7949405e6a31d1b057e1ca6777d1ab0
frankknoll <Knoll_Frank@web.de>
2024-11-07 08:09:15 +0100

    refactoring

--- app/src/test/java/de/KnollFrank/lib/settingssearch/FragmentsFactory.java
+++ app/src/test/java/de/KnollFrank/lib/settingssearch/FragmentsFactory.java
@@ -3,16 +3,22 @@ package de.KnollFrank.lib.settingssearch;
 import androidx.fragment.app.FragmentActivity;
 
 import de.KnollFrank.lib.settingssearch.fragment.DefaultFragmentFactory;
+import de.KnollFrank.lib.settingssearch.fragment.DefaultFragmentInitializer;
+import de.KnollFrank.lib.settingssearch.fragment.FragmentFactoryAndInitializer;
 import de.KnollFrank.lib.settingssearch.fragment.Fragments;
+import de.KnollFrank.lib.settingssearch.fragment.factory.FragmentFactoryAndInitializerWithCache;
 import de.KnollFrank.settingssearch.test.TestActivity;
 
-public class FragmentsFactory {
+class FragmentsFactory {
 
     public static Fragments createFragments(final FragmentActivity activity) {
-        return de.KnollFrank.lib.settingssearch.fragment.FragmentsFactory.createFragments(
-                new DefaultFragmentFactory(),
-                activity,
-                activity.getSupportFragmentManager(),
-                TestActivity.FRAGMENT_CONTAINER_VIEW);
+        return new Fragments(
+                new FragmentFactoryAndInitializerWithCache(
+                        new FragmentFactoryAndInitializer(
+                                new DefaultFragmentFactory(),
+                                new DefaultFragmentInitializer(
+                                        activity.getSupportFragmentManager(),
+                                        TestActivity.FRAGMENT_CONTAINER_VIEW))),
+                activity);
     }
 }
