19b557f25bf9810f6d7e3c4fd4d841a13ed2ce05
Frank Knoll <Knoll_Frank@web.de>
2024-11-09 17:34:02 +0100

    reactivating better version of PreferencePathNavigator

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/fragment/PreferencePathNavigator.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/fragment/PreferencePathNavigator.java
@@ -1,9 +1,8 @@
 package de.KnollFrank.lib.settingssearch.fragment;
 
-import static de.KnollFrank.lib.settingssearch.db.preference.converter.SearchablePreferenceFromPOJOConverter.createPlainSearchablePreference;
-
 import android.content.Context;
 
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import java.util.List;
@@ -30,40 +29,42 @@ public class PreferencePathNavigator {
     }
 
     public PreferenceFragmentCompat navigatePreferencePath(final PreferencePath preferencePath) {
-        return navigatePreferences(preferencePath.preferences(), null, null);
+        return navigatePreferences(preferencePath.preferences(), Optional.empty());
     }
 
     private PreferenceFragmentCompat navigatePreferences(final List<SearchablePreferencePOJO> preferences,
-                                                         final PreferenceWithHost src,
-                                                         final PreferenceFragmentCompat uninitializedSrc) {
-        if (preferences.isEmpty()) {
-            return uninitializedSrc;
-        }
-        final SearchablePreferencePOJO preference = Lists.head(preferences);
-        final var host = hostByPreference.get(preference);
-        return navigatePreferences(
-                Lists.tail(preferences),
-                new PreferenceWithHost(
-                        createPlainSearchablePreference(preference, context),
-                        instantiateAndInitializePreferenceFragment(host, src)),
-                instantiateFragment(host, src));
+                                                         final Optional<PreferenceWithHost> src) {
+        return preferences.isEmpty() ?
+                src.orElseThrow().host() :
+                navigatePreferences(
+                        Lists.tail(preferences),
+                        Optional.of(getPreferenceWithHost(Lists.head(preferences), src)));
+    }
+
+    private PreferenceWithHost getPreferenceWithHost(final SearchablePreferencePOJO preference,
+                                                     final Optional<PreferenceWithHost> src) {
+        final Class<? extends PreferenceFragmentCompat> host = hostByPreference.get(preference);
+        final PreferenceFragmentCompat hostOfPreference = instantiateAndInitializePreferenceFragment(host, src);
+        return new PreferenceWithHost(
+                getPreferenceByKey(hostOfPreference, preference.key().orElseThrow()),
+                hostOfPreference);
     }
 
     private PreferenceFragmentCompat instantiateAndInitializePreferenceFragment(
             final Class<? extends PreferenceFragmentCompat> preferenceFragment,
-            final PreferenceWithHost src) {
+            final Optional<PreferenceWithHost> src) {
         return (PreferenceFragmentCompat) fragmentFactoryAndInitializer.instantiateAndInitializeFragment(
                 preferenceFragment.getName(),
-                Optional.ofNullable(src),
+                src,
                 context);
     }
 
-    private PreferenceFragmentCompat instantiateFragment(
-            final Class<? extends PreferenceFragmentCompat> preferenceFragment,
-            final PreferenceWithHost src) {
-        return (PreferenceFragmentCompat) fragmentFactoryAndInitializer.instantiateFragment(
-                preferenceFragment.getName(),
-                Optional.ofNullable(src),
-                context);
+    private static Preference getPreferenceByKey(final PreferenceFragmentCompat preferenceFragment,
+                                                 final String key) {
+        final Preference preference = preferenceFragment.findPreference(key);
+        if (preference == null) {
+            throw new IllegalArgumentException("can not find preference with key " + key + " within preferenceFragment " + preferenceFragment);
+        }
+        return preference;
     }
 }
