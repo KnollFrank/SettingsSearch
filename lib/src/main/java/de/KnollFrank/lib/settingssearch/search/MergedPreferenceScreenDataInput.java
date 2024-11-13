bdffdb49034020611fb81bdad88ccc6ca5cbaa0a
Frank Knoll <Knoll_Frank@web.de>
2024-11-13 17:38:13 +0100

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/search/MergedPreferenceScreenDataInput.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/search/MergedPreferenceScreenDataInput.java
@@ -2,7 +2,7 @@ package de.KnollFrank.lib.settingssearch.search;
 
 import java.io.File;
 
-public record MergedPreferenceScreenDataInput(File preferences,
-                                              File preferencePathByPreference,
-                                              File hostByPreference) {
+record MergedPreferenceScreenDataInput(File preferences,
+                                       File preferencePathByPreference,
+                                       File hostByPreference) {
 }
