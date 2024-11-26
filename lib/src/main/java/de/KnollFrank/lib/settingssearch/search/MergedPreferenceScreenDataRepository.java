7acd6adcbcb72a701e0165108a72b85c016771a3
frankknoll <Knoll_Frank@web.de>
2024-11-26 18:04:16 +0100

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/search/MergedPreferenceScreenDataRepository.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/search/MergedPreferenceScreenDataRepository.java
@@ -18,19 +18,24 @@ class MergedPreferenceScreenDataRepository {
             final Locale locale,
             final Context context,
             final IProgressDisplayer progressDisplayer) {
+        final MergedPreferenceScreenDataFiles dataFiles = computeAndPersistMergedPreferenceScreenDataFiles(computeMergedPreferenceScreenData, locale, context, progressDisplayer);
+        return MergedPreferenceScreenDataFileDAO.load(dataFiles);
+    }
+
+    private static MergedPreferenceScreenDataFiles computeAndPersistMergedPreferenceScreenDataFiles(
+            final Supplier<MergedPreferenceScreenData> computeMergedPreferenceScreenData,
+            final Locale locale,
+            final Context context,
+            final IProgressDisplayer progressDisplayer) {
         final File directory = new SearchDatabaseDirectoryIO(context).getAndMakeSearchDatabaseDirectory4Locale(locale);
         final MergedPreferenceScreenDataFiles dataFiles = getMergedPreferenceScreenDataFiles(directory);
         // FK-TODO: show progressBar only for computeAndPersistMergedPreferenceScreenData() and not for load()?
-        // FK-TODO: die Berechnung der dataFiles über eine API startbar machen zu jedem beliebigen Zeitpunkt, z.B. schon beim Appstart und nicht erst im letzmöglichen Augenblick wie hier.
-        if (exists(dataFiles)) {
-            progressDisplayer.displayProgress("loading search database");
-            return MergedPreferenceScreenDataFileDAO.load(dataFiles);
-        } else {
+        if (!exists(dataFiles)) {
             final MergedPreferenceScreenData mergedPreferenceScreenData = computeMergedPreferenceScreenData.get();
             progressDisplayer.displayProgress("persisting search database");
             MergedPreferenceScreenDataFileDAO.persist(mergedPreferenceScreenData, dataFiles);
-            return mergedPreferenceScreenData;
         }
+        return dataFiles;
     }
 
     private static MergedPreferenceScreenDataFiles getMergedPreferenceScreenDataFiles(final File directory) {
