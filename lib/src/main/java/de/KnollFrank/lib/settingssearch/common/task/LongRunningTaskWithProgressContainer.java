25a4aed8edaa4fc31c2c09705b83c9247b7ae219
frankknoll <Knoll_Frank@web.de>
2024-11-30 23:35:46 +0100

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/common/task/LongRunningTaskWithProgressContainer.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/common/task/LongRunningTaskWithProgressContainer.java
@@ -1,72 +1,36 @@
 package de.KnollFrank.lib.settingssearch.common.task;
 
-import android.os.AsyncTask;
 import android.view.View;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 import de.KnollFrank.lib.settingssearch.search.progress.ProgressUpdateListener;
 
-public class LongRunningTaskWithProgressContainer<V> extends AsyncTask<Void, String, V> {
+public class LongRunningTaskWithProgressContainer<V> extends LongRunningTask<V> {
 
-    private final Function<ProgressUpdateListener, V> doInBackground;
-    private final Consumer<V> onPostExecute;
     private final View progressContainer;
     private final OnUiThreadRunner onUiThreadRunner;
-    private final List<ProgressUpdateListener> progressUpdateListeners = new ArrayList<>();
 
     public LongRunningTaskWithProgressContainer(final Function<ProgressUpdateListener, V> doInBackground,
                                                 final Consumer<V> onPostExecute,
                                                 final View progressContainer,
                                                 final OnUiThreadRunner onUiThreadRunner) {
-        this.doInBackground = doInBackground;
-        this.onPostExecute = onPostExecute;
+        super(doInBackground, onPostExecute);
         this.progressContainer = progressContainer;
         this.onUiThreadRunner = onUiThreadRunner;
-    }
-
-    public void addProgressUpdateListener(final ProgressUpdateListener progressUpdateListener) {
-        synchronized (progressUpdateListeners) {
-            progressUpdateListeners.add(progressUpdateListener);
-        }
-    }
-
-    public void removeProgressUpdateListener(final ProgressUpdateListener progressUpdateListener) {
-        synchronized (progressUpdateListeners) {
-            progressUpdateListeners.remove(progressUpdateListener);
-        }
     }
 
     @Override
     protected void onPreExecute() {
         setVisibilityOfProgressContainer(View.VISIBLE);
-    }
-
-    @Override
-    protected V doInBackground(final Void... voids) {
-        try {
-            return doInBackground.apply(this::publishProgress);
-        } catch (final Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    protected void onProgressUpdate(final String... values) {
-        synchronized (progressUpdateListeners) {
-            for (final ProgressUpdateListener progressUpdateListener : progressUpdateListeners) {
-                progressUpdateListener.onProgressUpdate(values[0]);
-            }
-        }
+        super.onPreExecute();
     }
 
     @Override
     protected void onPostExecute(final V result) {
         setVisibilityOfProgressContainer(View.GONE);
-        onPostExecute.accept(result);
+        super.onPostExecute(result);
     }
 
     private void setVisibilityOfProgressContainer(final int visible) {
