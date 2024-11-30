d2beae6e660a4b295644d1bbc156ffed26ff5134
frankknoll <Knoll_Frank@web.de>
2024-11-30 23:41:54 +0100

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/common/task/Tasks.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/common/task/Tasks.java
@@ -13,9 +13,9 @@ public class Tasks {
 
     // FK-TODO: replace all params here and elsewhere with AsyncTask<Params, Progress, Result>
     public static void asynchronouslyWaitForTask1ThenExecuteTask2(
-            final Optional<LongRunningTask<MergedPreferenceScreenData>> task1,
+            final Optional<? extends LongRunningTask<MergedPreferenceScreenData>> task1,
             final OnUiThreadProgressDisplayer progressDisplayer,
-            final LongRunningTaskWithProgressContainer<MergedPreferenceScreen> task2) {
+            final AsyncTask<Void, String, MergedPreferenceScreen> task2) {
         final AsyncTask<Void, Void, Void> asyncTask =
                 new AsyncTask<>() {
 
@@ -29,17 +29,22 @@ public class Tasks {
     }
 
     private static void waitForTask1ThenExecuteTask2(
-            final Optional<LongRunningTask<MergedPreferenceScreenData>> task1,
+            final Optional<? extends LongRunningTask<MergedPreferenceScreenData>> task1,
             final OnUiThreadProgressDisplayer progressDisplayer,
-            final LongRunningTaskWithProgressContainer<MergedPreferenceScreen> task2) {
+            final AsyncTask<Void, String, MergedPreferenceScreen> task2) {
         task1.ifPresentOrElse(
                 _task1 -> {
-                    _task1.addProgressUpdateListener(progressDisplayer);
-                    waitFor(_task1);
-                    _task1.removeProgressUpdateListener(progressDisplayer);
+                    waitForTaskWhileDisplayingItsProgress(_task1, progressDisplayer);
                     task2.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
                 },
                 () -> task2.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR));
+    }
+
+    private static void waitForTaskWhileDisplayingItsProgress(final LongRunningTask<MergedPreferenceScreenData> task,
+                                                              final OnUiThreadProgressDisplayer progressDisplayer) {
+        task.addProgressUpdateListener(progressDisplayer);
+        waitFor(task);
+        task.removeProgressUpdateListener(progressDisplayer);
     }
 
     private static <Params, Progress, Result> void waitFor(final AsyncTask<Params, Progress, Result> task) {
