b40daff0d6d2e275fb414db3e77fd130d64f7802
frankknoll <Knoll_Frank@web.de>
2024-12-05 23:03:56 +0100

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/common/Comparators.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/common/Comparators.java
@@ -5,14 +5,18 @@ import java.util.Optional;
 
 public class Comparators {
 
-    public static <T extends Comparable<T>> Comparator<Optional<T>> emptiesLast(final Comparator<T> comparator) {
+    private static final int ARG1_LESS_THAN_ARG2 = -1;
+    private static final int ARG1_EQUAL_TO_ARG2 = 0;
+    private static final int ARG1_GREATER_THAN_ARG2 = +1;
+
+    public static <T> Comparator<Optional<T>> emptiesLast(final Comparator<T> comparator) {
         return (optional1, optional2) -> {
             if (optional1.isEmpty() && optional2.isEmpty()) {
-                return 0;
+                return ARG1_EQUAL_TO_ARG2;
             } else if (optional1.isEmpty()) {
-                return +1;
+                return ARG1_GREATER_THAN_ARG2;
             } else if (optional2.isEmpty()) {
-                return -1;
+                return ARG1_LESS_THAN_ARG2;
             } else {
                 return comparator.compare(optional1.get(), optional2.get());
             }
