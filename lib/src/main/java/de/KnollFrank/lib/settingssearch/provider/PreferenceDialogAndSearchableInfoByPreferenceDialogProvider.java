0b25f7517aedcbdac5995f220e392dc23639d0e2
frankknoll <Knoll_Frank@web.de>
2024-12-15 21:26:42 +0100

    Revert "adding a TODO"
    This reverts commit dbfa03cda2e80022a8f212cbb285c587639e421b.

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/provider/PreferenceDialogAndSearchableInfoByPreferenceDialogProvider.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/provider/PreferenceDialogAndSearchableInfoByPreferenceDialogProvider.java
@@ -6,6 +6,5 @@ import de.KnollFrank.lib.settingssearch.search.provider.SearchableInfoByPreferen
 
 public record PreferenceDialogAndSearchableInfoByPreferenceDialogProvider<T extends Fragment>(
         T preferenceDialog,
-        // FK-TODO: the interface SearchableInfoByPreferenceDialogProvider does not need a T parameter in it's only method, because it is always the above preferenceDialog attribute. Instead move the interface method into this record (after making it an interface)
         SearchableInfoByPreferenceDialogProvider<T> searchableInfoByPreferenceDialogProvider) {
 }
