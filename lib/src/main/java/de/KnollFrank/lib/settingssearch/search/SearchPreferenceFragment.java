18ac3f4eeba474479a91802c27d5bc9924384f11
frankknoll <Knoll_Frank@web.de>
2025-01-16 10:29:13 +0100

    correcting a typo

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/search/SearchPreferenceFragment.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/search/SearchPreferenceFragment.java
@@ -33,7 +33,7 @@ import de.KnollFrank.lib.settingssearch.search.ui.ProgressContainerUI;
 import de.KnollFrank.lib.settingssearch.search.ui.SearchPreferenceFragmentUI;
 import de.KnollFrank.lib.settingssearch.search.ui.SearchPreferenceFragmentUIBinding;
 
-// FK-TODO: let users of this library extends this class for their custom UI instead of using SearchPreferenceFragmentUI?
+// FK-TODO: let users of this library extend this class for their custom UI instead of using SearchPreferenceFragmentUI?
 public class SearchPreferenceFragment extends Fragment {
 
     private static final @IdRes int DUMMY_FRAGMENT_CONTAINER_VIEW = View.generateViewId();
