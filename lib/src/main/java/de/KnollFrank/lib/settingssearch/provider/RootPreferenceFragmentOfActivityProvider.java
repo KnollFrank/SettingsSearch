ccbcc628b48c2ca20d425fbd9507977b2ed20861
frankknoll <Knoll_Frank@web.de>
2025-01-22 20:06:10 +0100

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/provider/RootPreferenceFragmentOfActivityProvider.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/provider/RootPreferenceFragmentOfActivityProvider.java
@@ -4,6 +4,7 @@ import androidx.preference.PreferenceFragmentCompat;
 
 import java.util.Optional;
 
+@FunctionalInterface
 public interface RootPreferenceFragmentOfActivityProvider {
 
     Optional<Class<? extends PreferenceFragmentCompat>> getRootPreferenceFragmentOfActivity(final String classNameOfActivity);
