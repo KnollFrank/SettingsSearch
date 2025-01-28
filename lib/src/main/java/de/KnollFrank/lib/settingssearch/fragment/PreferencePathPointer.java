cdd04b799a2d4d247ccfca63816d550a74f2b4ef
frankknoll <Knoll_Frank@web.de>
2025-01-28 10:33:16 +0100

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/fragment/PreferencePathPointer.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/fragment/PreferencePathPointer.java
@@ -1,18 +1,16 @@
 package de.KnollFrank.lib.settingssearch.fragment;
 
-import java.util.Objects;
 import java.util.Optional;
 
 import de.KnollFrank.lib.settingssearch.PreferencePath;
 import de.KnollFrank.lib.settingssearch.common.Lists;
 import de.KnollFrank.lib.settingssearch.db.preference.pojo.SearchablePreference;
 
-public final class PreferencePathPointer {
+public record PreferencePathPointer(PreferencePath preferencePath,
+                                    int indexWithinPreferencePath) {
 
-    private final PreferencePath preferencePath;
-    private final int indexWithinPreferencePath;
-
-    public PreferencePathPointer(final PreferencePath preferencePath, final int indexWithinPreferencePath) {
+    public PreferencePathPointer(final PreferencePath preferencePath,
+                                 final int indexWithinPreferencePath) {
         this.preferencePath = preferencePath;
         this.indexWithinPreferencePath = indexWithinPreferencePath;
         if (_dereference().isEmpty()) {
@@ -31,35 +29,6 @@ public final class PreferencePathPointer {
                                 preferencePath,
                                 indexWithinPreferencePath + 1)) :
                 Optional.empty();
-    }
-
-    public PreferencePath preferencePath() {
-        return preferencePath;
-    }
-
-    public int indexWithinPreferencePath() {
-        return indexWithinPreferencePath;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (PreferencePathPointer) obj;
-        return Objects.equals(this.preferencePath, that.preferencePath) &&
-                this.indexWithinPreferencePath == that.indexWithinPreferencePath;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(preferencePath, indexWithinPreferencePath);
-    }
-
-    @Override
-    public String toString() {
-        return "PreferencePathPointer[" +
-                "preferencePath=" + preferencePath + ", " +
-                "indexWithinPreferencePath=" + indexWithinPreferencePath + ']';
     }
 
     private Optional<SearchablePreference> _dereference() {
