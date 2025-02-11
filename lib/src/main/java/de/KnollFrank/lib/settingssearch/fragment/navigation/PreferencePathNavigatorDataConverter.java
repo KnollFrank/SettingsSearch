cfc754ee95ec75cdb19cc358a805ee1ce918576e
frankknoll <Knoll_Frank@web.de>
2025-02-11 08:03:26 +0100

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/fragment/navigation/PreferencePathNavigatorDataConverter.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/fragment/navigation/PreferencePathNavigatorDataConverter.java
@@ -3,31 +3,39 @@ package de.KnollFrank.lib.settingssearch.fragment.navigation;
 import android.os.Bundle;
 
 import java.util.Optional;
-import java.util.OptionalInt;
-
-import de.KnollFrank.lib.settingssearch.common.Bundles;
 
 class PreferencePathNavigatorDataConverter {
 
-    private static final String ID_OF_SEARCHABLE_PREFERENCE = PreferencePathNavigatorData.class.getPackage().getName() + ".idOfSearchablePreference";
-    private static final String INDEX_WITHIN_PREFERENCE_PATH = PreferencePathNavigatorData.class.getPackage().getName() + ".indexWithinPreferencePath";
+    private static final String PREFERENCE_PATH_NAVIGATOR_DATA = PreferencePathNavigatorData.class.getPackage().getName();
 
     public static Bundle toBundle(final PreferencePathNavigatorData preferencePathNavigatorData) {
         final Bundle bundle = new Bundle();
-        bundle.putInt(ID_OF_SEARCHABLE_PREFERENCE, preferencePathNavigatorData.idOfSearchablePreference());
-        bundle.putInt(INDEX_WITHIN_PREFERENCE_PATH, preferencePathNavigatorData.indexWithinPreferencePath());
+        bundle.putBundle(PREFERENCE_PATH_NAVIGATOR_DATA, Converter.toBundle(preferencePathNavigatorData));
         return bundle;
     }
 
     public static Optional<PreferencePathNavigatorData> fromBundle(final Bundle bundle) {
-        final Bundles bundles = new Bundles(bundle);
-        final OptionalInt idOfSearchablePreference = bundles.getOptionalInt(ID_OF_SEARCHABLE_PREFERENCE);
-        final OptionalInt indexWithinPreferencePath = bundles.getOptionalInt(INDEX_WITHIN_PREFERENCE_PATH);
-        return idOfSearchablePreference.isPresent() && indexWithinPreferencePath.isPresent() ?
-                Optional.of(
-                        new PreferencePathNavigatorData(
-                                idOfSearchablePreference.orElseThrow(),
-                                indexWithinPreferencePath.orElseThrow())) :
-                Optional.empty();
+        return Optional
+                .ofNullable(bundle.getBundle(PREFERENCE_PATH_NAVIGATOR_DATA))
+                .map(Converter::fromBundle);
+    }
+
+    private static class Converter {
+
+        private static final String ID_OF_SEARCHABLE_PREFERENCE = "idOfSearchablePreference";
+        private static final String INDEX_WITHIN_PREFERENCE_PATH = "indexWithinPreferencePath";
+
+        public static Bundle toBundle(final PreferencePathNavigatorData preferencePathNavigatorData) {
+            final Bundle bundle = new Bundle();
+            bundle.putInt(ID_OF_SEARCHABLE_PREFERENCE, preferencePathNavigatorData.idOfSearchablePreference());
+            bundle.putInt(INDEX_WITHIN_PREFERENCE_PATH, preferencePathNavigatorData.indexWithinPreferencePath());
+            return bundle;
+        }
+
+        public static PreferencePathNavigatorData fromBundle(final Bundle bundle) {
+            return new PreferencePathNavigatorData(
+                    bundle.getInt(ID_OF_SEARCHABLE_PREFERENCE),
+                    bundle.getInt(INDEX_WITHIN_PREFERENCE_PATH));
+        }
     }
 }
