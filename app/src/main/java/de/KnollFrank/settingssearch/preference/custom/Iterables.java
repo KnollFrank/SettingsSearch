a9f5e1cce9e59111d51286b3ea9626dc39247a5b
frankknoll <Knoll_Frank@web.de>
2025-02-21 17:46:32 +0100

    refactoring

--- app/src/main/java/de/KnollFrank/settingssearch/preference/custom/Iterables.java
+++ app/src/main/java/de/KnollFrank/settingssearch/preference/custom/Iterables.java
@@ -3,12 +3,13 @@ package de.KnollFrank.settingssearch.preference.custom;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.util.OptionalInt;
+import java.util.function.Predicate;
 
 public class Iterables {
 
-    public static <T extends @Nullable Object> OptionalInt indexOf(final Iterable<T> items,
-                                                                   final java.util.function.Predicate<? super T> predicate) {
-        final int index = com.google.common.collect.Iterables.indexOf(items, predicate::test);
+    public static <T extends @Nullable Object> OptionalInt indexOf(final Iterable<T> iterable,
+                                                                   final Predicate<? super T> predicate) {
+        final int index = com.google.common.collect.Iterables.indexOf(iterable, predicate::test);
         return index != -1 ? OptionalInt.of(index) : OptionalInt.empty();
     }
 }
