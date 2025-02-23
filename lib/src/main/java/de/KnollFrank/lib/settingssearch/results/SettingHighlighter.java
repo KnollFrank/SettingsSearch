1b295a668de3a4cbc43bf730278fe53035f9a2d5
Frank Knoll <Knoll_Frank@web.de>
2025-02-23 16:34:05 +0100

    removing a TODO

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/results/SettingHighlighter.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/results/SettingHighlighter.java
@@ -4,6 +4,5 @@ import androidx.fragment.app.Fragment;
 
 public interface SettingHighlighter {
 
-    // FK-TODO: add parameter "final Duration highlightDuration"?
     void highlightSetting(final Fragment settingsFragment, final Setting setting);
 }
