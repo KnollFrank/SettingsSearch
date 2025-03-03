e8cc567184e6710157fbd8006837887537772c96
frankknoll <Knoll_Frank@web.de>
2025-03-03 10:25:04 +0100

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/client/searchDatabaseConfig/PrincipalAndProxy.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/client/searchDatabaseConfig/PrincipalAndProxy.java
@@ -3,7 +3,7 @@ package de.KnollFrank.lib.settingssearch.client.searchDatabaseConfig;
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceFragmentCompat;
 
-public record PrincipalAndProxy<F extends Fragment, P extends PreferenceFragmentCompat & InitializePreferenceFragmentWithFragmentBeforeOnCreate<F>>(
-        Class<F> principal,
-        Class<P> proxy) {
+public record PrincipalAndProxy<Principal extends Fragment, Proxy extends PreferenceFragmentCompat & InitializePreferenceFragmentWithFragmentBeforeOnCreate<Principal>>(
+        Class<Principal> principal,
+        Class<Proxy> proxy) {
 }
