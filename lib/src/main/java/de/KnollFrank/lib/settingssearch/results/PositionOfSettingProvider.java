2b1b0783425a8d8047886e807aa973c35a6f6d8f
frankknoll <Knoll_Frank@web.de>
2025-03-08 22:19:40 +0100

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/results/PositionOfSettingProvider.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/results/PositionOfSettingProvider.java
@@ -2,6 +2,7 @@ package de.KnollFrank.lib.settingssearch.results;
 
 import java.util.OptionalInt;
 
+@FunctionalInterface
 public interface PositionOfSettingProvider {
 
     OptionalInt getPositionOfSetting(final Setting setting);
