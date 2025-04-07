bca5a400a221c603397c076505c586de03488889
frankknoll <Knoll_Frank@web.de>
2025-04-07 10:06:21 +0200

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/fragment/DefaultFragmentInitializer.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/fragment/DefaultFragmentInitializer.java
@@ -3,6 +3,9 @@ package de.KnollFrank.lib.settingssearch.fragment;
 import androidx.annotation.IdRes;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.util.function.BiConsumer;
 
 import de.KnollFrank.lib.settingssearch.common.task.OnUiThreadRunner;
 
@@ -37,25 +40,24 @@ public class DefaultFragmentInitializer implements FragmentInitializer, Preferen
     }
 
     private void add(final Fragment fragment) {
-        onUiThreadRunner.runBlockingOnUiThread(
-                () -> {
-                    this
-                            .fragmentManager
-                            .beginTransaction()
-                            .add(containerViewId, fragment)
-                            .commitNow();
-                    return null;
-                });
+        executeOperationOnFragment(
+                (_fragment, fragmentTransaction) -> fragmentTransaction.add(containerViewId, _fragment),
+                fragment);
     }
 
     private void remove(final Fragment fragment) {
+        executeOperationOnFragment(
+                (_fragment, fragmentTransaction) -> fragmentTransaction.remove(_fragment),
+                fragment);
+    }
+
+    private void executeOperationOnFragment(final BiConsumer<Fragment, FragmentTransaction> operation,
+                                            final Fragment fragment) {
         onUiThreadRunner.runBlockingOnUiThread(
                 () -> {
-                    this
-                            .fragmentManager
-                            .beginTransaction()
-                            .remove(fragment)
-                            .commitNow();
+                    final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    operation.accept(fragment, fragmentTransaction);
+                    fragmentTransaction.commitNow();
                     return null;
                 });
     }
