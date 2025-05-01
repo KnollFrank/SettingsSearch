e87749be01d6c86bb27846343aefa83eb3238d5b
frankknoll <Knoll_Frank@web.de>
2025-05-01 19:45:56 +0200

    formatting

--- app/src/test/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/converters/ConverterTest.java
+++ app/src/test/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/converters/ConverterTest.java
@@ -5,7 +5,10 @@ import static org.hamcrest.Matchers.is;
 
 class ConverterTest {
 
-    public static <A, B> void test_a_doForward_doBackward_equals_a(final Converter<A, B> converter, final A a) {
+    public static <A, B> void test_a_doForward_doBackward_equals_a(
+            // Given
+            final Converter<A, B> converter,
+            final A a) {
         // When
         final A aActual = converter.doBackward(converter.doForward(a));
 
