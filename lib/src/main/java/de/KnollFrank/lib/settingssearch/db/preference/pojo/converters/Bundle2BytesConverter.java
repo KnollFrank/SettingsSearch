c3d097c6a32ffee8a07186dc919fd4427ec12c40
Frank Knoll <Knoll_Frank@web.de>
2025-05-01 14:49:56 +0200

    adapting a TODO

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/converters/Bundle2BytesConverter.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/converters/Bundle2BytesConverter.java
@@ -9,7 +9,7 @@ import java.io.ByteArrayOutputStream;
 
 import de.KnollFrank.lib.settingssearch.db.preference.dao.JsonDAO;
 
-// FK-TODO: use new interface Converter<Bundle, byte[]>
+// FK-TODO: remove de.KnollFrank.lib.settingssearch.db.preference.pojo.converters.Converter and use com.google.common.base.Converter instead
 class Bundle2BytesConverter {
 
     public byte[] bundle2Bytes(final Bundle bundle) {
