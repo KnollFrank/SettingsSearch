b53e155d3f510fc0b7f1f1dbbcf0656f264f26c7
frankknoll <Knoll_Frank@web.de>
2025-05-11 17:50:06 +0200

    refactoring BundleWithEquality using Gemini

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/BundleWithEquality.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/BundleWithEquality.java
@@ -2,20 +2,28 @@ package de.KnollFrank.lib.settingssearch.db.preference.pojo;
 
 import android.os.Bundle;
 
+import java.util.Map;
 import java.util.StringJoiner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public record BundleWithEquality(Bundle bundle) {
 
     @Override
-    public boolean equals(final Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        final BundleWithEquality that = (BundleWithEquality) o;
-        return bundle.toString().equals(that.bundle.toString());
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        final BundleWithEquality that = (BundleWithEquality) obj;
+        return extractWrappedMap().equals(that.extractWrappedMap());
     }
 
     @Override
     public int hashCode() {
-        return bundle.toString().hashCode();
+        return extractWrappedMap().hashCode();
     }
 
     @Override
@@ -23,5 +31,19 @@ public record BundleWithEquality(Bundle bundle) {
         return new StringJoiner(", ", BundleWithEquality.class.getSimpleName() + "[", "]")
                 .add("bundle=" + bundle)
                 .toString();
+    }
+
+    private Map<String, Object> extractWrappedMap() {
+        return bundle
+                .keySet()
+                .stream()
+                .collect(
+                        Collectors.toMap(
+                                Function.identity(),
+                                key -> wrapBundle(bundle.get(key))));
+    }
+
+    private static Object wrapBundle(final Object obj) {
+        return obj instanceof final Bundle bundle ? new BundleWithEquality(bundle) : obj;
     }
 }
