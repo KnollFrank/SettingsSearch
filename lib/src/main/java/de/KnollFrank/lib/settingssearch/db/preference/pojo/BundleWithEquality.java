fb1180353f6396a653997b4a7cf46e7da7326e41
frankknoll <Knoll_Frank@web.de>
2025-05-11 13:52:58 +0200

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/BundleWithEquality.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/BundleWithEquality.java
@@ -4,21 +4,18 @@ import android.os.Bundle;
 
 import java.util.StringJoiner;
 
-import de.KnollFrank.lib.settingssearch.db.preference.pojo.converters.BundleConverter;
-import de.KnollFrank.lib.settingssearch.db.preference.pojo.converters.Converter;
-
 public record BundleWithEquality(Bundle bundle) {
 
     @Override
     public boolean equals(final Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         final BundleWithEquality that = (BundleWithEquality) o;
-        return asString(bundle).equals(asString(that.bundle));
+        return bundle.toString().equals(that.bundle.toString());
     }
 
     @Override
     public int hashCode() {
-        return asString(bundle).hashCode();
+        return bundle.toString().hashCode();
     }
 
     @Override
@@ -26,11 +23,5 @@ public record BundleWithEquality(Bundle bundle) {
         return new StringJoiner(", ", BundleWithEquality.class.getSimpleName() + "[", "]")
                 .add("bundle=" + bundle)
                 .toString();
-    }
-
-    private static final Converter<Bundle, String> bundleConverter = new BundleConverter();
-
-    private static String asString(final Bundle bundle) {
-        return bundleConverter.doForward(bundle);
     }
 }
--- lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/converters/Converter.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/converters/Converter.java
@@ -1,6 +1,6 @@
 package de.KnollFrank.lib.settingssearch.db.preference.pojo.converters;
 
-public interface Converter<A, B> {
+interface Converter<A, B> {
 
     B doForward(A a);
 
