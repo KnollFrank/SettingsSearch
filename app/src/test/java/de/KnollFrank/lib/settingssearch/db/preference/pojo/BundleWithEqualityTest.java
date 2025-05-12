91f335bf968332a88422d8b399db50ac0f34e1e1
frankknoll <Knoll_Frank@web.de>
2025-05-12 10:26:20 +0200

    refining some tests

--- app/src/test/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/BundleWithEqualityTest.java
+++ app/src/test/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/BundleWithEqualityTest.java
@@ -16,10 +16,21 @@ public class BundleWithEqualityTest {
 
     @Test
     public void testEquals() {
+        final Bundle bundle1 = new Bundle();
+        bundle1.putString("someKeyA", "someValueA");
+        bundle1.putString("someKeyB", "someValueB");
+
+        final Bundle bundle2 = new Bundle();
+        bundle2.putString("someKeyB", "someValueB");
+        bundle2.putString("someKeyA", "someValueA");
+
         new EqualsTester()
                 .addEqualityGroup(
                         new BundleWithEquality(new Bundle()),
                         new BundleWithEquality(new Bundle()))
+                .addEqualityGroup(
+                        new BundleWithEquality(bundle1),
+                        new BundleWithEquality(bundle2))
                 .addEqualityGroup(
                         new BundleWithEquality(createBundle("someKey", "someValue")),
                         new BundleWithEquality(createBundle("someKey", "someValue")))
--- app/src/test/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/converters/BundleConverterTest.java
+++ app/src/test/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/converters/BundleConverterTest.java
@@ -3,9 +3,12 @@ package de.KnollFrank.lib.settingssearch.db.preference.pojo.converters;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static de.KnollFrank.lib.settingssearch.db.preference.dao.BundleTestFactory.createBundle;
+import static de.KnollFrank.lib.settingssearch.db.preference.dao.BundleTestFactory.createSomeBundleOfBundles;
 import static de.KnollFrank.lib.settingssearch.db.preference.pojo.BundleEquality.equalBundles;
 
 import android.os.Bundle;
+
+import com.google.common.testing.EqualsTester;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,5 +28,28 @@ public class BundleConverterTest {
 
         // Then
         assertThat(equalBundles(bundle, bundleActual), is(true));
+    }
+
+    @Test
+    public void testEqualsOfConvertedBundles() {
+        final Bundle bundle1 = new Bundle();
+        bundle1.putString("someKeyA", "someValueA");
+        bundle1.putString("someKeyB", "someValueB");
+
+        final Bundle bundle2 = new Bundle();
+        bundle2.putString("someKeyB", "someValueB");
+        bundle2.putString("someKeyA", "someValueA");
+
+        final Converter<Bundle, String> converter = new BundleConverter();
+
+        new EqualsTester()
+                .addEqualityGroup(
+                        converter.doForward(bundle1),
+                        converter.doForward(bundle2))
+                .addEqualityGroup(
+                        converter.doForward(createBundle("someKey", "someValue")))
+                .addEqualityGroup(
+                        converter.doForward(createSomeBundleOfBundles()))
+                .testEquals();
     }
 }
