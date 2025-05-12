52448e39fb6a9ba47d33d33364eea91e0feb09cd
frankknoll <Knoll_Frank@web.de>
2025-05-12 07:42:34 +0200

    formatting

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/MergedPreferenceScreen.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/MergedPreferenceScreen.java
@@ -3,7 +3,6 @@ package de.KnollFrank.lib.settingssearch;
 import de.KnollFrank.lib.settingssearch.db.preference.dao.SearchablePreferenceDAO;
 import de.KnollFrank.lib.settingssearch.results.SearchResultsDisplayer;
 
-public record MergedPreferenceScreen(
-        SearchablePreferenceDAO searchablePreferenceDAO,
-        SearchResultsDisplayer searchResultsDisplayer) {
+public record MergedPreferenceScreen(SearchablePreferenceDAO searchablePreferenceDAO,
+                                     SearchResultsDisplayer searchResultsDisplayer) {
 }
