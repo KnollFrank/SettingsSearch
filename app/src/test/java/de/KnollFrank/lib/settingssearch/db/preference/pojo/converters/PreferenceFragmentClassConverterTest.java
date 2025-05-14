da1720a81682344904cba57a58f9a8816e19f988
Frank Knoll <Knoll_Frank@web.de>
2025-05-14 16:09:22 +0200

    refactoring

--- app/src/test/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/converters/PreferenceFragmentClassConverterTest.java
+++ app/src/test/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/converters/PreferenceFragmentClassConverterTest.java
@@ -1,9 +1,6 @@
 package de.KnollFrank.lib.settingssearch.db.preference.pojo.converters;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-
-import androidx.preference.PreferenceFragmentCompat;
+import static de.KnollFrank.lib.settingssearch.db.preference.pojo.converters.ConverterTest.test_a_doForward_doBackward_equals_a;
 
 import org.junit.Test;
 
@@ -11,17 +8,8 @@ import de.KnollFrank.settingssearch.preference.fragment.PrefsFragmentFirst;
 
 public class PreferenceFragmentClassConverterTest {
 
-    // FK-TODO: use ConverterTest?
     @Test
     public void shouldConvertFromClass2StringAndBack() {
-        // Given
-        final Converter<Class<? extends PreferenceFragmentCompat>, String> converter = new PreferenceFragmentClassConverter();
-        final Class<? extends PreferenceFragmentCompat> clazz = PrefsFragmentFirst.class;
-
-        // When
-        final Class<? extends PreferenceFragmentCompat> clazzActual = converter.doBackward(converter.doForward(clazz));
-
-        // Then
-        assertThat(clazzActual == clazz, is(true));
+        test_a_doForward_doBackward_equals_a(PrefsFragmentFirst.class, new PreferenceFragmentClassConverter());
     }
 }
