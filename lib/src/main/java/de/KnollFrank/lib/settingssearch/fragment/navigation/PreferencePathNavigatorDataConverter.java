5dfe6c89e5a97dd97493a52e0c25e2fc28a2d636
frankknoll <Knoll_Frank@web.de>
2025-05-14 13:07:33 +0200

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/fragment/navigation/PreferencePathNavigatorDataConverter.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/fragment/navigation/PreferencePathNavigatorDataConverter.java
@@ -4,37 +4,37 @@ import android.os.Bundle;
 
 import java.util.Optional;
 
-// FK-TODO: implement interface de.KnollFrank.lib.settingssearch.db.preference.pojo.converters.Converter
 class PreferencePathNavigatorDataConverter {
 
     private static final String PREFERENCE_PATH_NAVIGATOR_DATA = PreferencePathNavigatorData.class.getPackage().getName();
 
     public static Bundle toBundle(final PreferencePathNavigatorData preferencePathNavigatorData) {
         final Bundle bundle = new Bundle();
-        bundle.putBundle(PREFERENCE_PATH_NAVIGATOR_DATA, Converter.toBundle(preferencePathNavigatorData));
+        bundle.putBundle(PREFERENCE_PATH_NAVIGATOR_DATA, new Converter().doForward(preferencePathNavigatorData));
         return bundle;
     }
 
     public static Optional<PreferencePathNavigatorData> fromBundle(final Bundle bundle) {
         return Optional
                 .ofNullable(bundle.getBundle(PREFERENCE_PATH_NAVIGATOR_DATA))
-                .map(Converter::fromBundle);
+                .map(new Converter()::doBackward);
     }
 
-    // FK-TODO: implement interface de.KnollFrank.lib.settingssearch.db.preference.pojo.converters.Converter
-    private static class Converter {
+    private static class Converter implements de.KnollFrank.lib.settingssearch.db.preference.pojo.converters.Converter<PreferencePathNavigatorData, Bundle> {
 
         private static final String ID_OF_SEARCHABLE_PREFERENCE = "idOfSearchablePreference";
         private static final String INDEX_WITHIN_PREFERENCE_PATH = "indexWithinPreferencePath";
 
-        public static Bundle toBundle(final PreferencePathNavigatorData preferencePathNavigatorData) {
+        @Override
+        public Bundle doForward(final PreferencePathNavigatorData preferencePathNavigatorData) {
             final Bundle bundle = new Bundle();
             bundle.putInt(ID_OF_SEARCHABLE_PREFERENCE, preferencePathNavigatorData.idOfSearchablePreference());
             bundle.putInt(INDEX_WITHIN_PREFERENCE_PATH, preferencePathNavigatorData.indexWithinPreferencePath());
             return bundle;
         }
 
-        public static PreferencePathNavigatorData fromBundle(final Bundle bundle) {
+        @Override
+        public PreferencePathNavigatorData doBackward(final Bundle bundle) {
             return new PreferencePathNavigatorData(
                     bundle.getInt(ID_OF_SEARCHABLE_PREFERENCE),
                     bundle.getInt(INDEX_WITHIN_PREFERENCE_PATH));
