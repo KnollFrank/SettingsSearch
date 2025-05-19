a4b7fc6b20f0fc801d57ded2c001585c8e5697a5
frankknoll <Knoll_Frank@web.de>
2025-05-19 22:57:32 +0200

    updating guava to newer version

--- app/src/main/java/de/KnollFrank/settingssearch/preference/custom/Iterables.java
+++ app/src/main/java/de/KnollFrank/settingssearch/preference/custom/Iterables.java
@@ -2,7 +2,7 @@ package de.KnollFrank.settingssearch.preference.custom;
 
 import static de.KnollFrank.lib.settingssearch.common.IndexSearchResultConverter.minusOne2Empty;
 
-import org.checkerframework.checker.nullness.qual.Nullable;
+import org.jspecify.annotations.Nullable;
 
 import java.util.OptionalInt;
 import java.util.function.Predicate;
