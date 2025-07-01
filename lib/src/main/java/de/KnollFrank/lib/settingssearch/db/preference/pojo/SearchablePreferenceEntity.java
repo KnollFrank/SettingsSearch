a57773b0961507f4394d9897719c37ccc54bdd70
frankknoll <Knoll_Frank@web.de>
2025-07-01 12:16:26 +0200

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/SearchablePreferenceEntity.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/SearchablePreferenceEntity.java
@@ -1,13 +1,7 @@
 package de.KnollFrank.lib.settingssearch.db.preference.pojo;
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.drawable.Drawable;
-
 import androidx.annotation.LayoutRes;
-import androidx.appcompat.content.res.AppCompatResources;
 import androidx.room.Entity;
-import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 import com.codepoetics.ambivalence.Either;
@@ -16,10 +10,6 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.StringJoiner;
-import java.util.function.Supplier;
-
-import de.KnollFrank.lib.settingssearch.common.Classes;
-import de.KnollFrank.lib.settingssearch.common.converter.DrawableAndStringConverter;
 
 @Entity
 public final class SearchablePreferenceEntity {
@@ -37,25 +27,14 @@ public final class SearchablePreferenceEntity {
     private final int id;
     private final String key;
     private final Optional<Either<Integer, String>> iconResourceIdOrIconPixelData;
-    @Ignore
-    private Optional<Optional<Drawable>> iconCache = Optional.empty();
     private final @LayoutRes int layoutResId;
-
     private final Optional<String> title;
-    @Ignore
-    private Supplier<Optional<CharSequence>> highlightedTitleProvider = Optional::empty;
-
     private final Optional<String> summary;
-    @Ignore
-    private Supplier<Optional<CharSequence>> highlightedSummaryProvider = Optional::empty;
-
     private final @LayoutRes int widgetLayoutResId;
     private final Optional<String> fragment;
     private final Optional<String> classNameOfReferencedActivity;
     private final boolean visible;
     private final Optional<String> searchableInfo;
-    @Ignore
-    private Supplier<Optional<CharSequence>> highlightedSearchableInfoProvider = Optional::empty;
     private final Optional<Integer> parentId;
     private final Optional<Integer> predecessorId;
     private final String searchablePreferenceScreenId;
@@ -102,14 +81,6 @@ public final class SearchablePreferenceEntity {
         return iconResourceIdOrIconPixelData;
     }
 
-    // FK-TODO: remove
-    public Optional<Drawable> getIcon(final Context context) {
-        if (iconCache.isEmpty()) {
-            iconCache = Optional.of(_getIcon(context));
-        }
-        return iconCache.orElseThrow();
-    }
-
     public @LayoutRes int getLayoutResId() {
         return layoutResId;
     }
@@ -118,46 +89,12 @@ public final class SearchablePreferenceEntity {
         return summary;
     }
 
-    // FK-TODO: remove
-    public void setHighlightedSummaryProvider(final Supplier<Optional<CharSequence>> highlightedSummaryProvider) {
-        this.highlightedSummaryProvider = highlightedSummaryProvider;
-    }
-
-    // FK-TODO: remove
-    public Optional<CharSequence> getHighlightedSummary() {
-        return highlightedSummaryProvider.get();
-    }
-
     public Optional<String> getTitle() {
         return title;
     }
 
-    // FK-TODO: remove
-    public void setHighlightedTitleProvider(final Supplier<Optional<CharSequence>> highlightedTitleProvider) {
-        this.highlightedTitleProvider = highlightedTitleProvider;
-    }
-
-    // FK-TODO: remove
-    public Optional<CharSequence> getHighlightedTitle() {
-        return highlightedTitleProvider.get();
-    }
-
     public Optional<String> getSearchableInfo() {
         return searchableInfo;
-    }
-
-    // FK-TODO: remove
-    public void setHighlightedSearchableInfoProvider(final Supplier<Optional<CharSequence>> highlightedSearchableInfoProvider) {
-        this.highlightedSearchableInfoProvider = highlightedSearchableInfoProvider;
-    }
-
-    public Optional<CharSequence> getHighlightedSearchableInfo() {
-        return highlightedSearchableInfoProvider.get();
-    }
-
-    // FK-TODO: remove
-    public boolean hasPreferenceMatchWithinSearchableInfo() {
-        return getHighlightedSearchableInfo().isPresent();
     }
 
     public @LayoutRes int getWidgetLayoutResId() {
@@ -170,18 +107,6 @@ public final class SearchablePreferenceEntity {
 
     public Optional<String> getClassNameOfReferencedActivity() {
         return classNameOfReferencedActivity;
-    }
-
-    // FK-TODO: remove
-    public Optional<Class<? extends Activity>> getClassOfReferencedActivity(final Context context) {
-        return this
-                .getClassNameOfReferencedActivity()
-                .flatMap(
-                        classNameOfReferencedActivity ->
-                                Classes.classNameAsSubclassOfClazz(
-                                        classNameOfReferencedActivity,
-                                        Activity.class,
-                                        context));
     }
 
     public boolean isVisible() {
@@ -241,15 +166,4 @@ public final class SearchablePreferenceEntity {
                 .toString();
     }
 
-    private Optional<Drawable> _getIcon(final Context context) {
-        return this
-                .getIconResourceIdOrIconPixelData()
-                .map(iconResourceIdOrIconPixelData ->
-                             iconResourceIdOrIconPixelData.join(
-                                     iconResourceId -> AppCompatResources.getDrawable(context, iconResourceId),
-                                     iconPixelData ->
-                                             DrawableAndStringConverter.string2Drawable(
-                                                     iconPixelData,
-                                                     context.getResources())));
-    }
 }
