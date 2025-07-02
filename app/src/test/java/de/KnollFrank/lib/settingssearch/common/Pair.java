e665f6d7c8755a4b0a8fe0be8ba7b73c06f6a4a6
frankknoll <Knoll_Frank@web.de>
2025-07-02 08:18:47 +0200

    formatting

--- app/src/test/java/de/KnollFrank/lib/settingssearch/common/Pair.java
+++ app/src/test/java/de/KnollFrank/lib/settingssearch/common/Pair.java
@@ -1,6 +1,5 @@
 package de.KnollFrank.lib.settingssearch.common;
 
-// FK-TODO: remove this class and use android.util.Pair instead?
 public record Pair<F, S>(F first, S second) {
 
     public static <A, B> Pair<A, B> create(final A a, final B b) {
