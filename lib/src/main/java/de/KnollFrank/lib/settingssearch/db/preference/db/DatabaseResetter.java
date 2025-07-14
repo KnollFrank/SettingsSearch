a7990b6f6c7fb5598584d342867e97faad7e7a2f
frankknoll <Knoll_Frank@web.de>
2025-07-14 11:53:47 +0200

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/db/DatabaseResetter.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/db/DatabaseResetter.java
@@ -2,7 +2,7 @@ package de.KnollFrank.lib.settingssearch.db.preference.db;
 
 public class DatabaseResetter {
 
-    public static void resetDatabase(final AppDatabase appDatabase) {
-        appDatabase.searchablePreferenceScreenGraphDAO().removeAll();
+    public static void resetDatabase(final DAOProvider daoProvider) {
+        daoProvider.searchablePreferenceScreenGraphDAO().removeAll();
     }
 }
