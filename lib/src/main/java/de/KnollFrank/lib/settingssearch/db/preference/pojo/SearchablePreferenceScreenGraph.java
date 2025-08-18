296c9a108bcf76b54daa991ae085cac99885f2c3
Frank Knoll <Knoll_Frank@web.de>
2025-08-18 13:14:53 +0200

    updating a TODO

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/SearchablePreferenceScreenGraph.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/SearchablePreferenceScreenGraph.java
@@ -9,7 +9,9 @@ FK-TODO:
 + 1. a) für einige (z.B. häufig verwendete) Locales den SearchablePreferenceScreenGraph vorberechnen (und speichern)
      b) für andere Locales den SearchablePreferenceScreenGraph zur Laufzeit berechnen
 - 2. für jedes Plugin, welches eigene Preferences hat, seinen lokalisierten Teilgraph zur Laufzeit berechnen und in den SearchablePreferenceScreenGraph einhängen, analog zu PrefsFragmentFirst.subtreeReplacer.replaceSubtreeWithTree()
-- 3. teste in der Navigationsapp 1. a) und b)
+- 3. teste in der Navigationsapp
+     + 1. a) und
+     - 1. b)
 */
 public record SearchablePreferenceScreenGraph(
         Graph<SearchablePreferenceScreen, SearchablePreferenceEdge> graph,
