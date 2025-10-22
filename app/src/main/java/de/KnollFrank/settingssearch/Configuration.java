a26d0e02fbbb3ed5d22b713c5ac35998f1e78bda
frankknoll <Knoll_Frank@web.de>
2025-10-22 09:54:34 +0200

    refactoring

--- app/src/main/java/de/KnollFrank/settingssearch/Configuration.java
+++ app/src/main/java/de/KnollFrank/settingssearch/Configuration.java
@@ -2,4 +2,10 @@ package de.KnollFrank.settingssearch;
 
 public record Configuration(boolean addPreferenceToPreferenceFragmentWithSinglePreference,
                             boolean summaryChangingPreference) {
+
+    public Configuration asConfigurationHavingSummaryChangingPreference(final boolean summaryChangingPreference) {
+        return new Configuration(
+                addPreferenceToPreferenceFragmentWithSinglePreference,
+                summaryChangingPreference);
+    }
 }
