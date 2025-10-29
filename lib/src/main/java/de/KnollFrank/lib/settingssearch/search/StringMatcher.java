145525da68a3e24fe6c8e0ca82cab87d219d0c84
Frank Knoll <Knoll_Frank@web.de>
2025-10-29 22:27:27 +0100

    removing a TODO

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/search/StringMatcher.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/search/StringMatcher.java
@@ -3,7 +3,6 @@ package de.KnollFrank.lib.settingssearch.search;
 import java.util.Set;
 
 @FunctionalInterface
-// FK-TODO: Suche nicht nur mit indexOf(), sondern ähnlich zur Suche in AndroidStudio, z.B. Strg-Alt-T, dann "mergedscreen" eingeben, es wird dann sogar MergedPreferenceScreen gefunden und die passenden Teile gehighlightet.
 public interface StringMatcher {
 
     Set<IndexRange> getMatches(String haystack, String needle);
