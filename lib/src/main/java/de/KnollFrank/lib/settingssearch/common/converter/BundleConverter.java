5b4095cb94127bef38710bb70a1a99df0b06762b
frankknoll <Knoll_Frank@web.de>
2025-11-05 21:11:05 +0100

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/common/converter/BundleConverter.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/common/converter/BundleConverter.java
@@ -5,6 +5,8 @@ import android.os.PersistableBundle;
 
 import com.google.gson.Gson;
 
+import java.util.Optional;
+
 public class BundleConverter {
 
     private static final Gson gson = new Gson();
@@ -12,43 +14,48 @@ public class BundleConverter {
     public static PersistableBundle toPersistableBundle(final Bundle bundle) {
         final PersistableBundle persistableBundle = new PersistableBundle();
         for (final String key : bundle.keySet()) {
-            final Object value = bundle.get(key);
-            if (value == null) {
-                persistableBundle.putString(key, "<null>");
-                continue;
-            }
-            if (value instanceof final String s) {
-                persistableBundle.putString(key, s);
-            } else if (value instanceof final Integer i) {
-                persistableBundle.putInt(key, i);
-            } else if (value instanceof final Long l) {
-                persistableBundle.putLong(key, l);
-            } else if (value instanceof final Double d) {
-                persistableBundle.putDouble(key, d);
-            } else if (value instanceof final Boolean b) {
-                persistableBundle.putBoolean(key, b);
-            } else if (value instanceof final String[] sa) {
-                persistableBundle.putStringArray(key, sa);
-            } else if (value instanceof final int[] ia) {
-                persistableBundle.putIntArray(key, ia);
-            } else if (value instanceof final long[] la) {
-                persistableBundle.putLongArray(key, la);
-            } else if (value instanceof final double[] da) {
-                persistableBundle.putDoubleArray(key, da);
-            } else if (value instanceof final boolean[] ba) {
-                persistableBundle.putBooleanArray(key, ba);
-            } else if (value instanceof final PersistableBundle pb) {
-                persistableBundle.putPersistableBundle(key, pb);
-            } else if (value instanceof final Bundle b) {
-                persistableBundle.putPersistableBundle(key, toPersistableBundle(b));
-            } else {
-                try {
-                    persistableBundle.putString(key, gson.toJson(value));
-                } catch (final Exception e) {
-                    persistableBundle.putString(key, "<Unserializable type: " + value.getClass().getName() + ">");
-                }
-            }
+            Optional
+                    .ofNullable(bundle.get(key))
+                    .ifPresentOrElse(
+                            value -> putValueForKeyIntoBundle(key, value, persistableBundle),
+                            () -> persistableBundle.putString(key, "<null>"));
         }
         return persistableBundle;
+    }
+
+    private static void putValueForKeyIntoBundle(final String key,
+                                                 final Object value,
+                                                 final PersistableBundle bundle) {
+        if (value instanceof final String s) {
+            bundle.putString(key, s);
+        } else if (value instanceof final Integer i) {
+            bundle.putInt(key, i);
+        } else if (value instanceof final Long l) {
+            bundle.putLong(key, l);
+        } else if (value instanceof final Double d) {
+            bundle.putDouble(key, d);
+        } else if (value instanceof final Boolean b) {
+            bundle.putBoolean(key, b);
+        } else if (value instanceof final String[] sa) {
+            bundle.putStringArray(key, sa);
+        } else if (value instanceof final int[] ia) {
+            bundle.putIntArray(key, ia);
+        } else if (value instanceof final long[] la) {
+            bundle.putLongArray(key, la);
+        } else if (value instanceof final double[] da) {
+            bundle.putDoubleArray(key, da);
+        } else if (value instanceof final boolean[] ba) {
+            bundle.putBooleanArray(key, ba);
+        } else if (value instanceof final PersistableBundle pb) {
+            bundle.putPersistableBundle(key, pb);
+        } else if (value instanceof final Bundle b) {
+            bundle.putPersistableBundle(key, toPersistableBundle(b));
+        } else {
+            try {
+                bundle.putString(key, gson.toJson(value));
+            } catch (final Exception e) {
+                bundle.putString(key, "<Unserializable type: " + value.getClass().getName() + ">");
+            }
+        }
     }
 }
