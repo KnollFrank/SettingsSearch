6a4fbebc6ba700507db6d86d656f1282fba68562
frankknoll <Knoll_Frank@web.de>
2025-12-08 08:39:18 +0100

    removing a done TODO

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/PreferenceWithScreen.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/PreferenceWithScreen.java
@@ -5,7 +5,6 @@ import androidx.room.Embedded;
 public record PreferenceWithScreen(
         @Embedded
         SearchablePreferenceEntity preference,
-        // FK-TODO: extract constant for "screen_" and use in it here and in @Query of SearchablePreferenceScreenEntityDAO.
         @Embedded(prefix = SCREEN_PREFIX)
         SearchablePreferenceScreenEntity screen) {
 
