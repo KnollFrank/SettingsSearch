df079a704301d036d3db0e4df09e7782e6b66dfd
frankknoll <Knoll_Frank@web.de>
2026-01-08 11:12:40 +0100

    refactoring

--- app/src/test/java/de/KnollFrank/lib/settingssearch/common/graph/GraphConvertersTest.java
+++ app/src/test/java/de/KnollFrank/lib/settingssearch/common/graph/GraphConvertersTest.java
@@ -7,7 +7,6 @@ import static de.KnollFrank.lib.settingssearch.common.graph.StringGraphEquality.
 import com.google.common.graph.ImmutableValueGraph;
 import com.google.common.graph.ValueGraphBuilder;
 
-import org.jgrapht.Graph;
 import org.jgrapht.graph.AsUnmodifiableGraph;
 import org.junit.Test;
 
@@ -16,73 +15,49 @@ public class GraphConvertersTest {
 
     private final StringVertex vA = new StringVertex("A");
     private final StringVertex vB = new StringVertex("B");
-    private final StringVertex vC = new StringVertex("C");
+
+    // [A] --("Edge-AB")--> [B]
+    private final AsUnmodifiableGraph<StringVertex, StringEdge> jGraphT_GRAPH =
+            new AsUnmodifiableGraph<>(
+                    StringGraphs
+                            .newGraphBuilder()
+                            .addVertex(vA)
+                            .addVertex(vB)
+                            .addEdge(vA, vB, new StringEdge("Edge-AB"))
+                            .build());
+
+    // [A] --("Edge-AB")--> [B]
+    private final ImmutableValueGraph<StringVertex, String> GUAVA_GRAPH =
+            ValueGraphBuilder
+                    .directed()
+                    .<StringVertex, String>immutable()
+                    .addNode(vA)
+                    .addNode(vB)
+                    .putEdgeValue(vA, vB, "Edge-AB")
+                    .build();
 
     @Test
     public void shouldConvertJGraphTToGuava() {
-        // Given
-        /*
-         * Input JGraphT Graph:
-         * [A] --("Edge-AB")--> [B]
-         */
-        final Graph<StringVertex, StringEdge> jgraphtGraph =
-                StringGraphs
-                        .newGraphBuilder()
-                        .addVertex(vA)
-                        .addVertex(vB)
-                        .addEdge(vA, vB, new StringEdge("Edge-AB"))
-                        .build();
-
         // When
         final ImmutableValueGraph<StringVertex, String> guavaGraph =
                 GraphConverters.toGuava(
-                        jgraphtGraph,
+                        jGraphT_GRAPH,
                         StringEdge::getLabel);
 
         // Then
-        assertThat(
-                guavaGraph,
-                is(
-                        ValueGraphBuilder
-                                .directed()
-                                .<StringVertex, String>immutable()
-                                .addNode(vA)
-                                .addNode(vB)
-                                .putEdgeValue(vA, vB, "Edge-AB")
-                                .build()));
+        assertThat(guavaGraph, is(GUAVA_GRAPH));
     }
 
     @Test
     public void shouldConvertGuavaToJGraphT() {
-        // Given
-        /*
-         * Input Guava ValueGraph:
-         * [A] --("Edge-AC")--> [C]
-         */
-        final ImmutableValueGraph<StringVertex, String> guavaGraph =
-                ValueGraphBuilder
-                        .directed()
-                        .<StringVertex, String>immutable()
-                        .addNode(vA)
-                        .addNode(vC)
-                        .putEdgeValue(vA, vC, "Edge-AC")
-                        .build();
-
         // When
         final AsUnmodifiableGraph<StringVertex, StringEdge> jGraphT =
                 GraphConverters.asJGraphT(
-                        guavaGraph,
+                        GUAVA_GRAPH,
                         StringEdge.class,
                         StringEdge::new);
 
         // Then
-        assertActualEqualsExpected(
-                jGraphT,
-                StringGraphs
-                        .newGraphBuilder()
-                        .addVertex(vA)
-                        .addVertex(vC)
-                        .addEdge(vA, vC, new StringEdge("Edge-AC"))
-                        .build());
+        assertActualEqualsExpected(jGraphT, jGraphT);
     }
 }
