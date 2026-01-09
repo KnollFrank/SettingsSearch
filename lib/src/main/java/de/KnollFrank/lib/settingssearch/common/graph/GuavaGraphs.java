86cabbdf7a82c5333e8aef5b9827d2f7f25aca96
frankknoll <Knoll_Frank@web.de>
2026-01-09 10:49:32 +0100

    refactoring

---
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/common/graph/GuavaGraphs.java
@@ -0,0 +1,31 @@
+package de.KnollFrank.lib.settingssearch.common.graph;
+
+import com.google.common.collect.MoreCollectors;
+import com.google.common.graph.ValueGraph;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@SuppressWarnings({"UnstableApiUsage", "NullableProblems"})
+public class GuavaGraphs {
+
+    public static <Node> Optional<Node> getRootNode(final ValueGraph<Node, ?> graph) {
+        return GuavaGraphs
+                .getRootNodes(graph)
+                .stream()
+                .collect(MoreCollectors.toOptional());
+    }
+
+    public static <Node> Set<Node> getRootNodes(final ValueGraph<Node, ?> graph) {
+        return graph
+                .nodes()
+                .stream()
+                .filter(node -> isRootNode(graph, node))
+                .collect(Collectors.toSet());
+    }
+
+    private static <Node> boolean isRootNode(final ValueGraph<Node, ?> graph, final Node node) {
+        return graph.inDegree(node) == 0;
+    }
+}
--- lib/src/main/java/de/KnollFrank/lib/settingssearch/common/graph/GuavaTreeValidator.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/common/graph/GuavaTreeValidator.java
@@ -2,10 +2,12 @@ package de.KnollFrank.lib.settingssearch.common.graph;
 
 import com.google.common.graph.ValueGraph;
 
+import java.util.Set;
+
 @SuppressWarnings({"UnstableApiUsage", "NullableProblems"})
 class GuavaTreeValidator {
 
-    public static <V, W> TreeValidatorResult validateIsTree(final ValueGraph<V, W> graph) {
+    public static <Node, Value> TreeValidatorResult validateIsTree(final ValueGraph<Node, Value> graph) {
         // A tree must be a directed graph.
         if (!graph.isDirected()) {
             return TreeValidatorResult.invalid("A Tree must be a directed graph.");
@@ -13,35 +15,20 @@ class GuavaTreeValidator {
         if (isEmpty(graph)) {
             return TreeValidatorResult.invalid("The graph must not be empty.");
         }
-        int rootCount = 0;
-        for (final V node : graph.nodes()) {
-            final int inDegree = graph.inDegree(node);
-
-            // Each node must have an in-degree of at most 1.
-            if (inDegree > 1) {
+        {
+            final Set<Node> rootNodes = GuavaGraphs.getRootNodes(graph);
+            if (rootNodes.size() != 1) {
                 return TreeValidatorResult.invalid(
                         String.format(
-                                "Node '%s' has more than one parent (in-degree: %d). A tree is not allowed to have nodes with in-degree > 1.",
-                                node,
-                                inDegree));
-            }
-
-            // Count nodes with in-degree 0 (potential roots).
-            if (inDegree == 0) {
-                rootCount++;
+                                "A Tree must have exactly one root node, but found %d: %s",
+                                rootNodes.size(),
+                                rootNodes));
             }
         }
-
-        // There must be exactly one root.
-        if (rootCount != 1) {
-            return TreeValidatorResult.invalid(
-                    "A tree must have exactly one root (a node with in-degree 0), but found: " + rootCount);
-        }
-
         return TreeValidatorResult.valid();
     }
 
-    private static <V, W> boolean isEmpty(final ValueGraph<V, W> graph) {
+    private static <Node, Value> boolean isEmpty(final ValueGraph<Node, Value> graph) {
         return graph.nodes().isEmpty();
     }
 }
