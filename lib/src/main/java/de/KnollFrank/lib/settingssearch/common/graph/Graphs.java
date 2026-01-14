835b18e03835694208f827ea0ebfa695137fbb3b
frankknoll <Knoll_Frank@web.de>
2026-01-14 22:03:26 +0100

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/common/graph/Graphs.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/common/graph/Graphs.java
@@ -2,6 +2,7 @@ package de.KnollFrank.lib.settingssearch.common.graph;
 
 import com.google.common.collect.MoreCollectors;
 import com.google.common.graph.EndpointPair;
+import com.google.common.graph.ImmutableValueGraph;
 import com.google.common.graph.MutableValueGraph;
 import com.google.common.graph.ValueGraph;
 import com.google.common.graph.ValueGraphBuilder;
@@ -51,18 +52,18 @@ public class Graphs {
                 isOutgoingEdgeOfNode);
     }
 
-    public static <N, V> MutableValueGraph<N, V> toMutableValueGraph(final ValueGraph<N, V> graph) {
+    public static <N, V> MutableValueGraph<N, V> toMutableValueGraph(final ImmutableValueGraph<N, V> graph) {
         final MutableValueGraph<N, V> mutableCopy = ValueGraphBuilder.from(graph).build();
         copyNodes(graph, mutableCopy);
         copyEdges(graph, mutableCopy);
         return mutableCopy;
     }
 
-    private static <N, V> void copyNodes(final ValueGraph<N, V> src, final MutableValueGraph<N, V> dst) {
+    private static <N, V> void copyNodes(final ImmutableValueGraph<N, V> src, final MutableValueGraph<N, V> dst) {
         src.nodes().forEach(dst::addNode);
     }
 
-    private static <N, V> void copyEdges(final ValueGraph<N, V> src, final MutableValueGraph<N, V> dst) {
+    private static <N, V> void copyEdges(final ImmutableValueGraph<N, V> src, final MutableValueGraph<N, V> dst) {
         for (final EndpointPair<N> edge : src.edges()) {
             dst.putEdgeValue(
                     edge.source(),
