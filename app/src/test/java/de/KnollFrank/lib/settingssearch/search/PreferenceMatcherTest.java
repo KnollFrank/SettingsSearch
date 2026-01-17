94e272f658ada07f458921e88270d59e95ae15bc
frankknoll <Knoll_Frank@web.de>
2026-01-17 21:16:19 +0100

    refactoring

--- app/src/test/java/de/KnollFrank/lib/settingssearch/search/PreferenceMatcherTest.java
+++ app/src/test/java/de/KnollFrank/lib/settingssearch/search/PreferenceMatcherTest.java
@@ -5,57 +5,42 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static de.KnollFrank.lib.settingssearch.db.preference.pojo.SearchablePreferenceTestFactory.createSearchablePreference;
 import static de.KnollFrank.lib.settingssearch.db.preference.pojo.SearchablePreferenceWithinGraphTestFactory.createSearchablePreferenceWithinGraph;
 
-import androidx.test.core.app.ActivityScenario;
-
-import com.codepoetics.ambivalence.Either;
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
 
 import java.util.Optional;
 import java.util.Set;
 
-import de.KnollFrank.lib.settingssearch.common.converter.DrawableAndStringConverter;
 import de.KnollFrank.lib.settingssearch.db.preference.pojo.SearchablePreference;
-import de.KnollFrank.settingssearch.R;
-import de.KnollFrank.settingssearch.test.TestActivity;
 
-@RunWith(RobolectricTestRunner.class)
 public class PreferenceMatcherTest {
 
     @Test
     public void shouldGetPreferenceMatches() {
-        try (final ActivityScenario<TestActivity> scenario = ActivityScenario.launch(TestActivity.class)) {
-            scenario.onActivity(activity -> {
-                // Given
-                final PreferenceMatcher preferenceMatcher = new PreferenceMatcher(new CaseInsensitiveSubstringMatcher());
-                final SearchablePreference searchablePreference =
-                        createSearchablePreference(
-                                Optional.of("Title, title part"),
-                                Optional.of("title in summary"),
-                                Optional.of("searchable info also has a title"),
-                                // FK-TODO: braucht man wirklich einen Smiley und eine activity?
-                                Optional.of(Either.ofRight(DrawableAndStringConverter.drawable2String(activity.getResources().getDrawable(R.drawable.smiley, null)))));
+        // Given
+        final PreferenceMatcher preferenceMatcher = new PreferenceMatcher(new CaseInsensitiveSubstringMatcher());
+        final SearchablePreference searchablePreference =
+                createSearchablePreference(
+                        Optional.of("Title, title part"),
+                        Optional.of("title in summary"),
+                        Optional.of("searchable info also has a title"),
+                        Optional.empty());
 
-                // When
-                final Optional<PreferenceMatch> preferenceMatch =
-                        preferenceMatcher.getPreferenceMatch(
+        // When
+        final Optional<PreferenceMatch> preferenceMatch =
+                preferenceMatcher.getPreferenceMatch(
+                        createSearchablePreferenceWithinGraph(searchablePreference),
+                        "title");
+
+        // Then
+        assertThat(
+                preferenceMatch,
+                is(Optional.of(
+                        new PreferenceMatch(
                                 createSearchablePreferenceWithinGraph(searchablePreference),
-                                "title");
-
-                // Then
-                assertThat(
-                        preferenceMatch,
-                        is(Optional.of(
-                                new PreferenceMatch(
-                                        createSearchablePreferenceWithinGraph(searchablePreference),
-                                        Set.of(
-                                                new IndexRange(0, 5),
-                                                new IndexRange(7, 12)),
-                                        Set.of(new IndexRange(0, 5)),
-                                        Set.of(new IndexRange(27, 32))))));
-            });
-        }
+                                Set.of(
+                                        new IndexRange(0, 5),
+                                        new IndexRange(7, 12)),
+                                Set.of(new IndexRange(0, 5)),
+                                Set.of(new IndexRange(27, 32))))));
     }
 }
