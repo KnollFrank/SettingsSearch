c88954dc426f6c621e796ab7561cb4285abcac3c
frankknoll <Knoll_Frank@web.de>
2026-01-24 11:29:59 +0100

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/db/TreeProcessorExecutor.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/db/TreeProcessorExecutor.java
@@ -5,12 +5,16 @@ import android.os.PersistableBundle;
 import androidx.fragment.app.FragmentActivity;
 
 import com.codepoetics.ambivalence.Either;
+import com.google.common.graph.ImmutableValueGraph;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+import de.KnollFrank.lib.settingssearch.common.graph.Tree;
 import de.KnollFrank.lib.settingssearch.db.preference.db.transformer.SearchablePreferenceScreenTreeCreator;
 import de.KnollFrank.lib.settingssearch.db.preference.db.transformer.SearchablePreferenceScreenTreeTransformer;
+import de.KnollFrank.lib.settingssearch.db.preference.pojo.SearchablePreference;
+import de.KnollFrank.lib.settingssearch.db.preference.pojo.SearchablePreferenceScreen;
 import de.KnollFrank.lib.settingssearch.db.preference.pojo.SearchablePreferenceScreenTree;
 import de.KnollFrank.lib.settingssearch.db.preference.pojo.converters.ConfigurationBundleConverter;
 
@@ -27,50 +31,77 @@ class TreeProcessorExecutor<C> {
             final List<SearchablePreferenceScreenTree<PersistableBundle>> trees,
             final C configuration,
             final FragmentActivity activityContext) {
-        return trees
-                .stream()
-                .map(tree -> applyTreeProcessorsToTree(treeProcessors, tree, configuration, activityContext))
-                .collect(Collectors.toList());
+        return this
+                .createProcessorExecutionContext(configuration, activityContext)
+                .applyTreeProcessorsToTrees(treeProcessors, trees);
     }
 
-    private SearchablePreferenceScreenTree<PersistableBundle> applyTreeProcessorsToTree(
-            final List<Either<SearchablePreferenceScreenTreeCreator<C>, SearchablePreferenceScreenTreeTransformer<C>>> treeProcessors,
-            final SearchablePreferenceScreenTree<PersistableBundle> tree,
+    private ProcessorExecutionContext<C> createProcessorExecutionContext(
             final C configuration,
             final FragmentActivity activityContext) {
-        return treeProcessors
-                .stream()
-                .reduce(
-                        tree,
-                        (currentTree, treeProcessor) ->
-                                applyTreeProcessorToTree(
-                                        treeProcessor,
-                                        currentTree,
-                                        configuration,
-                                        activityContext),
-                        (tree1, tree2) -> {
-                            throw new UnsupportedOperationException("Parallel stream not supported");
-                        });
+        return new ProcessorExecutionContext<>(configuration, configurationBundleConverter, activityContext);
     }
 
-    private SearchablePreferenceScreenTree<PersistableBundle> applyTreeProcessorToTree(
-            final Either<SearchablePreferenceScreenTreeCreator<C>, SearchablePreferenceScreenTreeTransformer<C>> treeProcessor,
-            final SearchablePreferenceScreenTree<PersistableBundle> tree,
-            final C configuration,
-            final FragmentActivity activityContext) {
-        return new SearchablePreferenceScreenTree<>(
-                treeProcessor.join(
-                        treeCreator ->
-                                treeCreator.createTree(
-                                        tree.locale(),
-                                        configuration,
-                                        activityContext),
-                        treeTransformer ->
-                                treeTransformer.transformSearchablePreferenceScreenTree(
-                                        tree.mapConfiguration(configurationBundleConverter::convertBackward),
-                                        configuration,
-                                        activityContext)),
-                tree.locale(),
-                configurationBundleConverter.convertForward(configuration));
+    private static class ProcessorExecutionContext<C> {
+
+        private final C configuration;
+        private final ConfigurationBundleConverter<C> configurationBundleConverter;
+        private final FragmentActivity activityContext;
+
+        public ProcessorExecutionContext(final C configuration,
+                                         final ConfigurationBundleConverter<C> configurationBundleConverter,
+                                         final FragmentActivity activityContext) {
+            this.configuration = configuration;
+            this.configurationBundleConverter = configurationBundleConverter;
+            this.activityContext = activityContext;
+        }
+
+        public List<SearchablePreferenceScreenTree<PersistableBundle>> applyTreeProcessorsToTrees(
+                final List<Either<SearchablePreferenceScreenTreeCreator<C>, SearchablePreferenceScreenTreeTransformer<C>>> treeProcessors,
+                final List<SearchablePreferenceScreenTree<PersistableBundle>> trees) {
+            return trees
+                    .stream()
+                    .map(tree -> applyTreeProcessorsToTree(treeProcessors, tree))
+                    .collect(Collectors.toList());
+        }
+
+        private SearchablePreferenceScreenTree<PersistableBundle> applyTreeProcessorsToTree(
+                final List<Either<SearchablePreferenceScreenTreeCreator<C>, SearchablePreferenceScreenTreeTransformer<C>>> treeProcessors,
+                final SearchablePreferenceScreenTree<PersistableBundle> tree) {
+            return treeProcessors
+                    .stream()
+                    .reduce(
+                            tree,
+                            (currentTree, treeProcessor) -> applyTreeProcessorToTree(treeProcessor, currentTree),
+                            (tree1, tree2) -> {
+                                throw new UnsupportedOperationException("Parallel stream not supported");
+                            });
+        }
+
+        private SearchablePreferenceScreenTree<PersistableBundle> applyTreeProcessorToTree(
+                final Either<SearchablePreferenceScreenTreeCreator<C>, SearchablePreferenceScreenTreeTransformer<C>> treeProcessor,
+                final SearchablePreferenceScreenTree<PersistableBundle> tree) {
+            return new SearchablePreferenceScreenTree<>(
+                    _applyTreeProcessorToTree(treeProcessor, tree),
+                    tree.locale(),
+                    configurationBundleConverter.convertForward(configuration));
+        }
+
+        @SuppressWarnings({"UnstableApiUsage", "NullableProblems"})
+        private Tree<SearchablePreferenceScreen, SearchablePreference, ImmutableValueGraph<SearchablePreferenceScreen, SearchablePreference>> _applyTreeProcessorToTree(
+                final Either<SearchablePreferenceScreenTreeCreator<C>, SearchablePreferenceScreenTreeTransformer<C>> treeProcessor,
+                final SearchablePreferenceScreenTree<PersistableBundle> tree) {
+            return treeProcessor.join(
+                    treeCreator ->
+                            treeCreator.createTree(
+                                    tree.locale(),
+                                    configuration,
+                                    activityContext),
+                    treeTransformer ->
+                            treeTransformer.transformSearchablePreferenceScreenTree(
+                                    tree.mapConfiguration(configurationBundleConverter::convertBackward),
+                                    configuration,
+                                    activityContext));
+        }
     }
 }
