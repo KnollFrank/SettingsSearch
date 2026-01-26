9781c6166ef4cbd8bbe91e5c163f5b94ba11bfdb
frankknoll <Knoll_Frank@web.de>
2026-01-26 22:43:47 +0100

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/converters/TreeProcessorClassConverter.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/converters/TreeProcessorClassConverter.java
@@ -21,27 +21,35 @@ public class TreeProcessorClassConverter implements Converter<Either<Class<? ext
         return creatorClassOrTransformerClass
                 .map(treeCreatorClassConverter::convertForward,
                      treeTransformerClassConverter::convertForward)
-                .join(className -> LEFT_PREFIX + className,
-                      className -> RIGHT_PREFIX + className);
+                .join(className -> wrapClassName(className, LEFT_PREFIX),
+                      className -> wrapClassName(className, RIGHT_PREFIX));
     }
 
     @TypeConverter
     @Override
-    public Either<Class<? extends SearchablePreferenceScreenTreeCreator<?>>, Class<? extends SearchablePreferenceScreenTreeTransformer<?>>> convertBackward(final String string) {
-        if (string.startsWith(LEFT_PREFIX)) {
-            return Either.ofLeft(
-                    treeCreatorClassConverter.convertBackward(
-                            getClassNameOfStringStringWithPrefix(string, LEFT_PREFIX)));
-        } else if (string.startsWith(RIGHT_PREFIX)) {
-            return Either.ofRight(
-                    treeTransformerClassConverter.convertBackward(
-                            getClassNameOfStringStringWithPrefix(string, RIGHT_PREFIX)));
+    public Either<Class<? extends SearchablePreferenceScreenTreeCreator<?>>, Class<? extends SearchablePreferenceScreenTreeTransformer<?>>> convertBackward(final String classNameStartingWithPrefix) {
+        return TreeProcessorClassConverter
+                .unwrapClassName(classNameStartingWithPrefix)
+                .map(treeCreatorClassConverter::convertBackward,
+                     treeTransformerClassConverter::convertBackward);
+
+    }
+
+    private static String wrapClassName(final String className, final String prefix) {
+        return prefix + className;
+    }
+
+    private static Either<String, String> unwrapClassName(final String classNameStartingWithPrefix) {
+        if (classNameStartingWithPrefix.startsWith(LEFT_PREFIX)) {
+            return Either.ofLeft(getClassName(classNameStartingWithPrefix, LEFT_PREFIX));
+        } else if (classNameStartingWithPrefix.startsWith(RIGHT_PREFIX)) {
+            return Either.ofRight(getClassName(classNameStartingWithPrefix, RIGHT_PREFIX));
         } else {
-            throw new IllegalArgumentException("Invalid value for TreeProcessorClassConverter: " + string);
+            throw new IllegalArgumentException("Invalid value for TreeProcessorClassConverter: " + classNameStartingWithPrefix);
         }
     }
 
-    private static String getClassNameOfStringStringWithPrefix(final String string, final String prefix) {
-        return string.substring(prefix.length());
+    private static String getClassName(final String classNameStartingWithPrefix, final String prefix) {
+        return classNameStartingWithPrefix.substring(prefix.length());
     }
 }
