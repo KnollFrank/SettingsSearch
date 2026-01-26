76d37640447b8a120f85d9725a774bafc4f44784
frankknoll <Knoll_Frank@web.de>
2026-01-26 18:59:21 +0100

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/converters/PreferenceFragmentClassConverter.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/pojo/converters/PreferenceFragmentClassConverter.java
@@ -1,22 +1,6 @@
 package de.KnollFrank.lib.settingssearch.db.preference.pojo.converters;
 
 import androidx.preference.PreferenceFragmentCompat;
-import androidx.room.TypeConverter;
 
-import de.KnollFrank.lib.settingssearch.common.Classes;
-
-// FK-TODO: use ClassConverter
-public class PreferenceFragmentClassConverter implements Converter<Class<? extends PreferenceFragmentCompat>, String> {
-
-    @TypeConverter
-    @Override
-    public String convertForward(final Class<? extends PreferenceFragmentCompat> clazz) {
-        return clazz.getName();
-    }
-
-    @TypeConverter
-    @Override
-    public Class<? extends PreferenceFragmentCompat> convertBackward(final String string) {
-        return Classes.getClass(string);
-    }
+public class PreferenceFragmentClassConverter extends ClassConverter<PreferenceFragmentCompat> implements Converter<Class<? extends PreferenceFragmentCompat>, String> {
 }
