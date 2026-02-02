a8d5563ab4037a0b49cc20bcd1c92eb833026191
frankknoll <Knoll_Frank@web.de>
2026-02-02 20:50:06 +0100

    adding a TODO

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/client/searchDatabaseConfig/ActivityDescription.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/client/searchDatabaseConfig/ActivityDescription.java
@@ -4,5 +4,6 @@ import android.app.Activity;
 import android.os.PersistableBundle;
 
 public record ActivityDescription(Class<? extends Activity> activity,
+                                  // FK-TODO: in der Datenbank sollte dies ein LazyPersistableBundle sein
                                   PersistableBundle arguments) {
 }
