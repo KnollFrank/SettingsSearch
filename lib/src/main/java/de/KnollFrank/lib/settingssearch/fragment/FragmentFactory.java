6c9ee652cd0ae98f4fdfa7b31e49b37a21b37f56
frankknoll <Knoll_Frank@web.de>
2026-02-05 11:07:05 +0100

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/fragment/FragmentFactory.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/fragment/FragmentFactory.java
@@ -14,7 +14,6 @@ import de.KnollFrank.lib.settingssearch.PreferenceOfHostOfActivity;
 public interface FragmentFactory {
 
     // instantiate fragmentClass, where fragmentClass.fragment().getName().equals(src.orElseThrow().preference.getFragment()) if src.isPresent()
-    // FK-TODO: für SearchDatabaseConfigBuilder.fragmentFactory sollte es eine FragmentFactory geben mit Rückgabetyp T statt FragmentOfActivity<T>
     <T extends Fragment> FragmentOfActivity<T> instantiate(
             FragmentClassOfActivity<T> fragmentClass,
             Optional<PreferenceOfHostOfActivity> src,
