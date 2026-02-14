47d96e2074b31495d49dda2f173e54b54fdd6b1a
frankknoll <Knoll_Frank@web.de>
2026-02-14 22:33:36 +0100

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/common/graph/TreePath.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/common/graph/TreePath.java
@@ -4,13 +4,20 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.graph.ValueGraph;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.StringJoiner;
 
 import de.KnollFrank.lib.settingssearch.common.Lists;
 
 @SuppressWarnings({"UnstableApiUsage", "NullableProblems"})
-public record TreePath<N, V, G extends ValueGraph<N, V>>(Tree<N, V, G> tree, List<N> nodes) {
+public final class TreePath<N, V, G extends ValueGraph<N, V>> {
 
-    public TreePath {
+    private final Tree<N, V, G> tree;
+    private final List<N> nodes;
+    private Optional<List<Edge<N, V>>> edgesOnPath = Optional.empty();
+
+    public TreePath(final Tree<N, V, G> tree, final List<N> nodes) {
         if (nodes.isEmpty()) {
             throw new IllegalArgumentException("Path nodes cannot be empty.");
         }
@@ -18,6 +25,8 @@ public record TreePath<N, V, G extends ValueGraph<N, V>>(Tree<N, V, G> tree, Lis
             throw new IllegalArgumentException("All nodes of the TreePath must be part of the tree.");
         }
         // FK-TODO: add assertion that nodes form a real path of the tree?
+        this.tree = tree;
+        this.nodes = nodes;
     }
 
     public TreePath<N, V, G> add(final N node) {
@@ -39,6 +48,37 @@ public record TreePath<N, V, G extends ValueGraph<N, V>>(Tree<N, V, G> tree, Lis
     }
 
     public List<Edge<N, V>> edges() {
-        return Trees.getEdgesOnPath(this);
+        if (edgesOnPath.isEmpty()) {
+            edgesOnPath = Optional.of(Trees.getEdgesOnPath(this));
+        }
+        return edgesOnPath.orElseThrow();
+    }
+
+    public Tree<N, V, G> tree() {
+        return tree;
+    }
+
+    public List<N> nodes() {
+        return nodes;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        final TreePath<?, ?, ?> treePath = (TreePath<?, ?, ?>) o;
+        return Objects.equals(tree, treePath.tree) && Objects.equals(nodes, treePath.nodes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tree, nodes);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", TreePath.class.getSimpleName() + "[", "]")
+                .add("tree=" + tree)
+                .add("nodes=" + nodes)
+                .toString();
     }
 }
