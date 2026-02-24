2cf0a39d91c3e75040549cb120de9589973293fd
frankknoll <Knoll_Frank@web.de>
2026-02-24 11:33:07 +0100

    adding a TODO

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/db/PreferencesDatabaseConfig.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/db/PreferencesDatabaseConfig.java
@@ -2,6 +2,7 @@ package de.KnollFrank.lib.settingssearch.db.preference.db;
 
 import java.util.Optional;
 
+// FK-TODO: warum nicht "File databaseFileName" statt String?
 public record PreferencesDatabaseConfig<C>(String databaseFileName,
                                            Optional<PrepackagedPreferencesDatabase<C>> prepackagedPreferencesDatabase,
                                            JournalMode journalMode) {
