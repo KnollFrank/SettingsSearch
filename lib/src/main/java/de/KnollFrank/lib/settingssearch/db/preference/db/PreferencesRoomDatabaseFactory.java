2ae2831b53c39f44513ddf3e6d9584a094eabe5c
frankknoll <Knoll_Frank@web.de>
2026-02-27 10:05:06 +0100

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/db/PreferencesRoomDatabaseFactory.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/db/PreferencesRoomDatabaseFactory.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import java.io.File;
+
 class PreferencesRoomDatabaseFactory {
 
     private PreferencesRoomDatabaseFactory() {
@@ -13,20 +15,25 @@ class PreferencesRoomDatabaseFactory {
     public static <C> PreferencesRoomDatabase createPreferencesRoomDatabase(
             final PreferencesDatabaseConfig<C> preferencesDatabaseConfig,
             final Context context) {
-        final RoomDatabase.Builder<PreferencesRoomDatabase> preferencesDatabaseBuilder =
-                Room
-                        .databaseBuilder(
-                                context.getApplicationContext(),
-                                PreferencesRoomDatabase.class,
-                                preferencesDatabaseConfig.databaseFileName())
-                        .setJournalMode(asRoomJournalMode(preferencesDatabaseConfig.journalMode()))
-                        // FK-TODO: remove allowMainThreadQueries()
-                        .allowMainThreadQueries();
-        preferencesDatabaseConfig
-                .prepackagedPreferencesDatabase()
-                .map(PrepackagedPreferencesDatabase::databaseAssetFile)
-                .ifPresent(databaseAssetFile -> preferencesDatabaseBuilder.createFromAsset(databaseAssetFile.getPath()));
-        return preferencesDatabaseBuilder.build();
+        final RoomDatabase.Builder<PreferencesRoomDatabase> databaseBuilder =
+                createPreferencesRoomDatabaseBuilder(
+                        preferencesDatabaseConfig,
+                        context);
+        maybeCreateFromAsset(databaseBuilder, preferencesDatabaseConfig, context);
+        return databaseBuilder.build();
+    }
+
+    private static <C> RoomDatabase.Builder<PreferencesRoomDatabase> createPreferencesRoomDatabaseBuilder(
+            final PreferencesDatabaseConfig<C> preferencesDatabaseConfig,
+            final Context context) {
+        return Room
+                .databaseBuilder(
+                        context.getApplicationContext(),
+                        PreferencesRoomDatabase.class,
+                        preferencesDatabaseConfig.databaseFileName())
+                .setJournalMode(asRoomJournalMode(preferencesDatabaseConfig.journalMode()))
+                // FK-TODO: remove allowMainThreadQueries()
+                .allowMainThreadQueries();
     }
 
     private static RoomDatabase.JournalMode asRoomJournalMode(final PreferencesDatabaseConfig.JournalMode journalMode) {
@@ -35,5 +42,17 @@ class PreferencesRoomDatabaseFactory {
             case TRUNCATE -> RoomDatabase.JournalMode.TRUNCATE;
             case WRITE_AHEAD_LOGGING -> RoomDatabase.JournalMode.WRITE_AHEAD_LOGGING;
         };
+    }
+
+    private static <C> void maybeCreateFromAsset(final RoomDatabase.Builder<PreferencesRoomDatabase> databaseBuilder,
+                                                 final PreferencesDatabaseConfig<C> preferencesDatabaseConfig,
+                                                 final Context context) {
+        final File databaseFile = context.getDatabasePath(preferencesDatabaseConfig.databaseFileName());
+        if (!databaseFile.exists()) {
+            preferencesDatabaseConfig
+                    .prepackagedPreferencesDatabase()
+                    .map(PrepackagedPreferencesDatabase::databaseAssetFile)
+                    .ifPresent(databaseAssetFile -> databaseBuilder.createFromAsset(databaseAssetFile.getPath()));
+        }
     }
 }
