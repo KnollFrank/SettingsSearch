9969416659b6a2e68044b623ad7028ee1e990747
frankknoll <Knoll_Frank@web.de>
2026-02-27 11:31:13 +0100

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/db/PreferencesRoomDatabaseFactory.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/db/preference/db/PreferencesRoomDatabaseFactory.java
@@ -21,62 +21,67 @@ class PreferencesRoomDatabaseFactory {
     public static <C> PreferencesRoomDatabase createPreferencesRoomDatabase(
             final PreferencesDatabaseConfig<C> preferencesDatabaseConfig,
             final Context context) {
-        final RoomDatabase.Builder<PreferencesRoomDatabase> databaseBuilder =
-                createPreferencesRoomDatabaseBuilder(
+        final var worker =
+                new PreferencesRoomDatabaseFactoryWorker<>(
                         preferencesDatabaseConfig,
                         context);
-        maybeCreateFromInputStreamCallable(databaseBuilder, preferencesDatabaseConfig, context);
-        return databaseBuilder.build();
+        return worker.createPreferencesRoomDatabase();
     }
 
-    private static <C> RoomDatabase.Builder<PreferencesRoomDatabase> createPreferencesRoomDatabaseBuilder(
-            final PreferencesDatabaseConfig<C> preferencesDatabaseConfig,
-            final Context context) {
-        return Room
-                .databaseBuilder(
-                        context.getApplicationContext(),
-                        PreferencesRoomDatabase.class,
-                        preferencesDatabaseConfig.databaseFileName())
-                .setJournalMode(asRoomJournalMode(preferencesDatabaseConfig.journalMode()))
-                // FK-TODO: remove allowMainThreadQueries()
-                .allowMainThreadQueries();
-    }
+    private static class PreferencesRoomDatabaseFactoryWorker<C> {
 
-    private static RoomDatabase.JournalMode asRoomJournalMode(final PreferencesDatabaseConfig.JournalMode journalMode) {
-        return switch (journalMode) {
-            case AUTOMATIC -> RoomDatabase.JournalMode.AUTOMATIC;
-            case TRUNCATE -> RoomDatabase.JournalMode.TRUNCATE;
-            case WRITE_AHEAD_LOGGING -> RoomDatabase.JournalMode.WRITE_AHEAD_LOGGING;
-        };
-    }
+        private final PreferencesDatabaseConfig<C> preferencesDatabaseConfig;
+        private final Context context;
 
-    private static <C> void maybeCreateFromInputStreamCallable(
-            final RoomDatabase.Builder<PreferencesRoomDatabase> databaseBuilder,
-            final PreferencesDatabaseConfig<C> preferencesDatabaseConfig,
-            final Context context) {
-        if (!existsDatabaseFile(preferencesDatabaseConfig.databaseFileName(), context)) {
-            PreferencesRoomDatabaseFactory
-                    .getInputStreamCallable(
-                            preferencesDatabaseConfig.prepackagedPreferencesDatabase(),
-                            context.getAssets())
-                    .ifPresent(databaseBuilder::createFromInputStream);
+        public PreferencesRoomDatabaseFactoryWorker(final PreferencesDatabaseConfig<C> preferencesDatabaseConfig, final Context context) {
+            this.preferencesDatabaseConfig = preferencesDatabaseConfig;
+            this.context = context;
         }
-    }
 
-    private static boolean existsDatabaseFile(final String databaseFileName, final Context context) {
-        final File databaseFile = context.getDatabasePath(databaseFileName);
-        return databaseFile.exists();
-    }
+        public PreferencesRoomDatabase createPreferencesRoomDatabase() {
+            final RoomDatabase.Builder<PreferencesRoomDatabase> databaseBuilder = createPreferencesRoomDatabaseBuilder();
+            maybeCreateFromInputStreamCallable(databaseBuilder);
+            return databaseBuilder.build();
+        }
 
-    private static <C> Optional<Callable<InputStream>> getInputStreamCallable(
-            final Optional<PrepackagedPreferencesDatabase<C>> prepackagedPreferencesDatabase,
-            final AssetManager assetManager) {
-        return prepackagedPreferencesDatabase
-                .map(PrepackagedPreferencesDatabase::databaseAssetFile)
-                .map(databaseAssetFile -> createInputStreamCallable(databaseAssetFile, assetManager));
-    }
+        private RoomDatabase.Builder<PreferencesRoomDatabase> createPreferencesRoomDatabaseBuilder() {
+            return Room
+                    .databaseBuilder(
+                            context.getApplicationContext(),
+                            PreferencesRoomDatabase.class,
+                            preferencesDatabaseConfig.databaseFileName())
+                    .setJournalMode(asRoomJournalMode(preferencesDatabaseConfig.journalMode()))
+                    // FK-TODO: remove allowMainThreadQueries()
+                    .allowMainThreadQueries();
+        }
 
-    private static Callable<InputStream> createInputStreamCallable(final File databaseAssetFile, final AssetManager assetManager) {
-        return () -> AssetsUtils.open(databaseAssetFile, assetManager);
+        private static RoomDatabase.JournalMode asRoomJournalMode(final PreferencesDatabaseConfig.JournalMode journalMode) {
+            return switch (journalMode) {
+                case AUTOMATIC -> RoomDatabase.JournalMode.AUTOMATIC;
+                case TRUNCATE -> RoomDatabase.JournalMode.TRUNCATE;
+                case WRITE_AHEAD_LOGGING -> RoomDatabase.JournalMode.WRITE_AHEAD_LOGGING;
+            };
+        }
+
+        private void maybeCreateFromInputStreamCallable(final RoomDatabase.Builder<PreferencesRoomDatabase> databaseBuilder) {
+            if (!getDatabaseFile().exists()) {
+                getInputStreamCallable().ifPresent(databaseBuilder::createFromInputStream);
+            }
+        }
+
+        private File getDatabaseFile() {
+            return context.getDatabasePath(preferencesDatabaseConfig.databaseFileName());
+        }
+
+        private Optional<Callable<InputStream>> getInputStreamCallable() {
+            return preferencesDatabaseConfig
+                    .prepackagedPreferencesDatabase()
+                    .map(PrepackagedPreferencesDatabase::databaseAssetFile)
+                    .map(databaseAssetFile -> createInputStreamCallable(databaseAssetFile, context.getAssets()));
+        }
+
+        private static Callable<InputStream> createInputStreamCallable(final File databaseAssetFile, final AssetManager assetManager) {
+            return () -> AssetsUtils.open(databaseAssetFile, assetManager);
+        }
     }
 }
