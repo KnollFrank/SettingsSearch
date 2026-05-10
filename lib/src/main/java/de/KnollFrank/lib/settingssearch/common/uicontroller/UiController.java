7aeb8fdb71cfebb6a6413d835d9dfac86294b5b3
frankknoll <Knoll_Frank@web.de>
2026-05-10 23:03:18 +0200

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/common/uicontroller/UiController.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/common/uicontroller/UiController.java
@@ -2,7 +2,6 @@ package de.KnollFrank.lib.settingssearch.common.uicontroller;
 
 import android.app.Activity;
 import android.os.Looper;
-import android.os.MessageQueue;
 import android.view.View;
 import android.view.ViewTreeObserver;
 
@@ -13,23 +12,22 @@ public class UiController {
     // FK-TODO: refactor
     public static void waitUntilIdle() throws InterruptedException {
         final Activity activity = CurrentActivityProvider.awaitResumedActivity();
+        waitUntilMainLooperIsIdle(activity);
+        waitUntilLayoutIsStable(activity);
+    }
 
-        // 1. Warten bis der Main-Looper idle ist (MessageQueue leer)
+    private static void waitUntilMainLooperIsIdle(final Activity activity) throws InterruptedException {
         final CountDownLatch looperLatch = new CountDownLatch(1);
         activity.runOnUiThread(
                 () ->
-                        Looper.myQueue().addIdleHandler(
-                                new MessageQueue.IdleHandler() {
-
-                                    @Override
-                                    public boolean queueIdle() {
-                                        looperLatch.countDown();
-                                        return false;
-                                    }
-                                }));
+                        Looper.myQueue().addIdleHandler(() -> {
+                            looperLatch.countDown();
+                            return false;
+                        }));
         looperLatch.await();
+    }
 
-        // 2. Warten bis das Layout stabil ist
+    private static void waitUntilLayoutIsStable(final Activity activity) throws InterruptedException {
         final CountDownLatch layoutLatch = new CountDownLatch(1);
         activity.runOnUiThread(() -> {
             final View decorView = activity.getWindow().getDecorView();
