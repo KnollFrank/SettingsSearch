7a6ee8780a371a4ae39d038b42887386410392cf
frankknoll <Knoll_Frank@web.de>
2026-05-10 23:50:28 +0200

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/common/uicontroller/UiController.java
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/common/uicontroller/UiController.java
@@ -7,9 +7,10 @@ import android.view.ViewTreeObserver;
 
 import java.util.concurrent.CountDownLatch;
 
+import de.KnollFrank.lib.settingssearch.common.task.OnUiThreadRunnerFactory;
+
 public class UiController {
 
-    // FK-TODO: refactor
     public static void waitUntilIdle() throws InterruptedException {
         final Activity activity = CurrentActivityProvider.awaitResumedActivity();
         waitUntilMainLooperIsIdle(activity);
@@ -18,33 +19,44 @@ public class UiController {
 
     private static void waitUntilMainLooperIsIdle(final Activity activity) throws InterruptedException {
         final CountDownLatch looperLatch = new CountDownLatch(1);
-        activity.runOnUiThread(
-                () ->
-                        Looper.myQueue().addIdleHandler(() -> {
-                            looperLatch.countDown();
-                            return false;
-                        }));
+        OnUiThreadRunnerFactory
+                .fromActivity(activity)
+                .runNonBlockingOnUiThread(
+                        () ->
+                                Looper
+                                        .myQueue()
+                                        .addIdleHandler(() -> {
+                                            looperLatch.countDown();
+                                            return false;
+                                        }));
         looperLatch.await();
     }
 
     private static void waitUntilLayoutIsStable(final Activity activity) throws InterruptedException {
-        final CountDownLatch layoutLatch = new CountDownLatch(1);
-        activity.runOnUiThread(() -> {
-            final View decorView = activity.getWindow().getDecorView();
-            if (!decorView.isLayoutRequested()) {
-                layoutLatch.countDown();
-                return;
-            }
-            decorView.getViewTreeObserver().addOnGlobalLayoutListener(
-                    new ViewTreeObserver.OnGlobalLayoutListener() {
+        final CountDownLatch layoutIsStableLatch = new CountDownLatch(1);
+        final boolean pending =
+                OnUiThreadRunnerFactory
+                        .fromActivity(activity)
+                        .runBlockingOnUiThread(() -> isLayoutOfViewPending(activity.getWindow().getDecorView(), layoutIsStableLatch));
+        if (pending) {
+            layoutIsStableLatch.await();
+        }
+    }
 
-                        @Override
-                        public void onGlobalLayout() {
-                            decorView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                            layoutLatch.countDown();
-                        }
-                    });
-        });
-        layoutLatch.await();
+    private static boolean isLayoutOfViewPending(final View view, final CountDownLatch layoutIsStableLatch) {
+        if (!view.isLayoutRequested()) {
+            return false;
+        }
+        final ViewTreeObserver viewTreeObserver = view.getViewTreeObserver();
+        viewTreeObserver.addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+
+                    @Override
+                    public void onGlobalLayout() {
+                        viewTreeObserver.removeOnGlobalLayoutListener(this);
+                        layoutIsStableLatch.countDown();
+                    }
+                });
+        return true;
     }
 }
