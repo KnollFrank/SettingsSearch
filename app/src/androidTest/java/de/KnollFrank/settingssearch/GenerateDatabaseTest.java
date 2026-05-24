70fad959106a7cf1fd6e173b3184b421fe1120ee
frankknoll <Knoll_Frank@web.de>
2026-05-24 10:28:17 +0200

    adding yet failing GenerateDatabaseTest.generateDatabaseViaUiCrawler()

--- app/src/androidTest/java/de/KnollFrank/settingssearch/GenerateDatabaseTest.java
+++ app/src/androidTest/java/de/KnollFrank/settingssearch/GenerateDatabaseTest.java
@@ -1,13 +1,23 @@
 package de.KnollFrank.settingssearch;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 import static de.KnollFrank.settingssearch.PreferenceSearchExampleTest.searchButton;
 import static de.KnollFrank.settingssearch.PreferenceSearchExampleTest.searchView;
 
+import androidx.fragment.app.FragmentActivity;
+import androidx.preference.Preference;
+import androidx.test.espresso.Espresso;
 import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -17,7 +27,17 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Locale;
+import java.util.concurrent.atomic.AtomicReference;
+
+import de.KnollFrank.lib.settingssearch.client.searchDatabaseConfig.SearchDatabaseConfig;
 import de.KnollFrank.lib.settingssearch.common.EspressoIdlingResource;
+import de.KnollFrank.lib.settingssearch.db.preference.converter.PreferenceScreenToSearchablePreferenceScreenConverter;
+import de.KnollFrank.lib.settingssearch.db.preference.converter.PreferenceToSearchablePreferenceConverterFactory;
+import de.KnollFrank.lib.settingssearch.db.preference.db.SearchablePreferenceScreenTreeRepository;
+import de.KnollFrank.lib.settingssearch.db.preference.pojo.SearchablePreferenceScreenTree;
+import de.KnollFrank.lib.settingssearch.fragment.PreferenceDialogsFactory;
+import de.KnollFrank.lib.settingssearch.graph.UiCrawler;
 
 @RunWith(AndroidJUnit4.class)
 public class GenerateDatabaseTest {
@@ -49,10 +69,82 @@ public class GenerateDatabaseTest {
         System.out.println("Database generation and search fragment display were successful.");
     }
 
+    // FK-TODO: refactor
+    @Test
+    public void generateDatabaseViaUiCrawler() {
+        final AtomicReference<FragmentActivity> activityRef = new AtomicReference<>();
+        activityRule.getScenario().onActivity(activityRef::set);
+        final FragmentActivity activity = activityRef.get();
+        final UiCrawler crawler =
+                new UiCrawler(
+                        createConverter(activity),
+                        new EspressoUiNavigator(),
+                        searchablePreference -> false);
+
+        System.out.println("Starting UI Crawl...");
+        final var tree = crawler.crawl();
+        System.out.println("UI Crawl completed.");
+
+        // Persist the tree
+        GenerateDatabaseTest
+                .getTreeRepository(activity)
+                .persistOrReplace(
+                        new SearchablePreferenceScreenTree<>(
+                                tree,
+                                Locale.getDefault(),
+                                new ConfigurationBundleConverter().convertForward(ConfigurationProvider.getActualConfiguration(activity))));
+        System.out.println("Database generated via UiCrawler and persisted successfully.");
+    }
+
     @After
     public void unregisterIdlingResource() {
         IdlingRegistry
                 .getInstance()
                 .unregister(EspressoIdlingResource.getCountingIdlingResource());
+    }
+
+    private static PreferenceScreenToSearchablePreferenceScreenConverter createConverter(final FragmentActivity activity) {
+        final SearchDatabaseConfig<Configuration> searchDatabaseConfig = SearchDatabaseConfigFactory.createSearchDatabaseConfig();
+        return new PreferenceScreenToSearchablePreferenceScreenConverter(
+                PreferenceToSearchablePreferenceConverterFactory.createPreferenceToSearchablePreferenceConverter(
+                        searchDatabaseConfig,
+                        PreferenceDialogsFactory.createPreferenceDialogs(
+                                activity,
+                                R.id.fragmentContainerView,
+                                searchDatabaseConfig.preferenceSearchablePredicate)));
+    }
+
+    private static SearchablePreferenceScreenTreeRepository<Configuration> getTreeRepository(final FragmentActivity activity) {
+        return SettingsSearchApplication
+                .getInstanceFromContext(activity)
+                .preferencesDatabaseManager
+                .getPreferencesDatabase()
+                .searchablePreferenceScreenTreeRepository();
+    }
+
+    private static class EspressoUiNavigator implements UiCrawler.UiNavigator {
+
+        @Override
+        public void click(final Preference preference) {
+            Espresso
+                    .onView(withId(androidx.preference.R.id.recycler_view))
+                    .perform(
+                            RecyclerViewActions.actionOnItem(
+                                    hasDescendant(
+                                            allOf(
+                                                    withId(android.R.id.title),
+                                                    withText(preference.getTitle().toString()))),
+                                    ViewActions.click()));
+        }
+
+        @Override
+        public void goBack() {
+            pressBack();
+        }
+
+        @Override
+        public void waitUntilIdle() {
+            // Espresso handles this automatically
+        }
     }
 }
---
+++ lib/src/main/java/de/KnollFrank/lib/settingssearch/graph/UiCrawler.java
@@ -0,0 +1,157 @@
+package de.KnollFrank.lib.settingssearch.graph;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.google.common.graph.ImmutableValueGraph;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import de.KnollFrank.lib.settingssearch.ActivityDescription;
+import de.KnollFrank.lib.settingssearch.PreferenceScreenOfHostOfActivity;
+import de.KnollFrank.lib.settingssearch.common.converter.BundleConverter;
+import de.KnollFrank.lib.settingssearch.common.graph.Tree;
+import de.KnollFrank.lib.settingssearch.common.uicontroller.Fragments;
+import de.KnollFrank.lib.settingssearch.db.preference.converter.PreferenceScreenToSearchablePreferenceScreenConverter;
+import de.KnollFrank.lib.settingssearch.db.preference.converter.SearchablePreferenceScreenWithMap;
+import de.KnollFrank.lib.settingssearch.db.preference.pojo.SearchablePreference;
+import de.KnollFrank.lib.settingssearch.db.preference.pojo.SearchablePreferenceScreen;
+
+// FK-TODO: refactor
+public final class UiCrawler {
+
+    public interface UiNavigator {
+
+        void click(final Preference preference);
+
+        void goBack();
+
+        void waitUntilIdle() throws InterruptedException;
+    }
+
+    private final PreferenceScreenToSearchablePreferenceScreenConverter converter;
+    private final UiNavigator uiNavigator;
+    private final Predicate<SearchablePreference> isSubScreenPredicate;
+
+    public UiCrawler(final PreferenceScreenToSearchablePreferenceScreenConverter converter,
+                     final UiNavigator uiNavigator,
+                     final Predicate<SearchablePreference> isSubScreenPredicate) {
+        this.converter = converter;
+        this.uiNavigator = uiNavigator;
+        this.isSubScreenPredicate = isSubScreenPredicate;
+    }
+
+    @SuppressWarnings({"UnstableApiUsage", "NullableProblems"})
+    public Tree<SearchablePreferenceScreen, SearchablePreference, ImmutableValueGraph<SearchablePreferenceScreen, SearchablePreference>> crawl() {
+        final TreeBuilder<SearchablePreferenceScreen, SearchablePreference> treeBuilder =
+                new TreeBuilder<>(
+                        new CrawlerListener(),
+                        this::createEdgeSuppliersHavingSource);
+        return treeBuilder.buildTreeWithRoot(getRootSearchablePreferenceScreen());
+    }
+
+    private List<EdgeSupplier<SearchablePreferenceScreen, SearchablePreference>> createEdgeSuppliersHavingSource(final SearchablePreferenceScreen screen) {
+        return screen
+                .allPreferencesOfPreferenceHierarchy()
+                .stream()
+                .filter(this::isSubScreen)
+                .map(searchablePreference -> EdgeSupplierFactory.createEdgeSupplier(screen, searchablePreference, () -> getTargetNode(screen, searchablePreference)))
+                .collect(Collectors.toList());
+    }
+
+    private boolean isSubScreen(final SearchablePreference searchablePreference) {
+        return isLikelySubScreen(searchablePreference) || isSubScreenPredicate.test(searchablePreference);
+    }
+
+    private Optional<SearchablePreferenceScreen> getTargetNode(final SearchablePreferenceScreen screen, final SearchablePreference searchablePreference) {
+        final PreferenceFragmentCompat currentFragment =
+                Fragments
+                        .findVisiblePreferenceFragmentOnCurrentActivity()
+                        .orElseThrow(() -> new IllegalStateException("Current fragment not found during crawl"));
+
+        // Wir konvertieren den Screen jedes Mal neu, um die aktuellen View-Referenzen zu erhalten,
+        // da wir zwischenzeitlich in Unterseiten und wieder zurück navigiert sind.
+        final SearchablePreferenceScreenWithMap screenWithMap =
+                converter.convertPreferenceScreen(
+                        createPreferenceScreenOfHostOfActivity(currentFragment),
+                        screen.id());
+        final Preference preference = screenWithMap.pojoEntityMap().get(searchablePreference);
+        if (preference == null) {
+            return Optional.empty();
+        }
+        // Jetzt erst wird physisch geklickt!
+        uiNavigator.click(preference);
+        try {
+            uiNavigator.waitUntilIdle();
+        } catch (final InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+        final Optional<PreferenceFragmentCompat> childFragment = Fragments.findVisiblePreferenceFragmentOnCurrentActivity();
+        // Prüfen, ob wir wirklich auf einer neuen Seite gelandet sind
+        if (childFragment.isPresent() && childFragment.get() != currentFragment) {
+            return Optional.of(
+                    converter
+                            .convertPreferenceScreen(
+                                    createPreferenceScreenOfHostOfActivity(childFragment.get()),
+                                    screen.id() + "/" + searchablePreference.getKey())
+                            .searchablePreferenceScreen());
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    private SearchablePreferenceScreen getRootSearchablePreferenceScreen() {
+        return converter
+                .convertPreferenceScreen(
+                        createPreferenceScreenOfHostOfActivity(getRootPreferenceFragment()),
+                        "root")
+                .searchablePreferenceScreen();
+    }
+
+    private static PreferenceFragmentCompat getRootPreferenceFragment() {
+        return Fragments
+                .findVisiblePreferenceFragmentOnCurrentActivity()
+                .orElseThrow(() -> new IllegalStateException("No visible PreferenceFragment found."));
+    }
+
+    private class CrawlerListener extends TreeBuilderListeners.EmptyTreeBuilderListener<SearchablePreferenceScreen, SearchablePreference> {
+
+        @Override
+        public void onFinishBuildSubtree(final SearchablePreferenceScreen subtreeRoot, final boolean isRootOfTree) {
+            if (isRootOfTree) {
+                return;
+            }
+            uiNavigator.goBack();
+            try {
+                uiNavigator.waitUntilIdle();
+            } catch (final InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    private static boolean isLikelySubScreen(final SearchablePreference searchablePreference) {
+        return searchablePreference.getFragment().isPresent() ||
+                searchablePreference.getClassNameOfReferencedActivity().isPresent();
+    }
+
+    private static PreferenceScreenOfHostOfActivity createPreferenceScreenOfHostOfActivity(final PreferenceFragmentCompat preferenceFragment) {
+        final Activity activity = preferenceFragment.requireActivity();
+        return new PreferenceScreenOfHostOfActivity(
+                preferenceFragment.getPreferenceScreen(),
+                preferenceFragment,
+                new ActivityDescription(
+                        activity.getClass(),
+                        BundleConverter.toPersistableBundle(
+                                Optional
+                                        .ofNullable(activity.getIntent().getExtras())
+                                        .orElse(new Bundle()))));
+    }
+}
