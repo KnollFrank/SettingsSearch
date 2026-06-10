d17c5a35b7317226f28e7201accc9b518b1a2e35
frankknoll <Knoll_Frank@web.de>
2026-06-10 08:51:39 +0200

    refactoring

--- lib/src/main/java/de/KnollFrank/lib/settingssearch/client/searchDatabaseConfig/InitializePreferenceFragmentWithIntentOfActivityBeforeOnCreate.java
+++
@@ -1,8 +0,0 @@
-package de.KnollFrank.lib.settingssearch.client.searchDatabaseConfig;
-
-import android.content.Intent;
-
-public interface InitializePreferenceFragmentWithIntentOfActivityBeforeOnCreate {
-
-    void initializePreferenceFragmentWithIntentOfActivityBeforeOnCreate(Intent intentOfActivity);
-}
